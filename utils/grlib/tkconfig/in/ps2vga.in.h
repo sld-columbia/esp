d4ef6b059bd6cc95b2cfe1e9e3d9a40e19402917
Paolo Mantovani <paolo@cs.columbia.edu>
2016-10-13 14:18:12 -0400

    utils/grlib/tkconfig/in/ps2vga: add configuration for hardware frame buffer

--- utils/grlib/tkconfig/in/ps2vga.in.h
+++ utils/grlib/tkconfig/in/ps2vga.in.h
@@ -7,4 +7,6 @@
 #ifndef CONFIG_KBD_ENABLE
 #define CONFIG_KBD_ENABLE 0
 #endif
-
+#ifndef CONFIG_SVGA_MEMORY_HADDR
+#define CONFIG_SVGA_MEMORY_HADDR B01
+#endif
--- utils/grlib/tkconfig/in/ps2vga.in.vhd
+++ utils/grlib/tkconfig/in/ps2vga.in.vhd
@@ -2,4 +2,5 @@
   constant CFG_KBD_ENABLE  : integer := CONFIG_KBD_ENABLE;
   constant CFG_VGA_ENABLE  : integer := CONFIG_VGA_ENABLE;
   constant CFG_SVGA_ENABLE : integer := CONFIG_SVGA_ENABLE;
+  constant CFG_SVGA_MEMORY_HADDR : integer := 16#CONFIG_SVGA_MEMORY_HADDR#;
 
