3372ad36bc19b053dff08d7fddcef9942d5a9e45
Paolo Mantovani <paolo@cs.columbia.edu>
2017-02-21 16:48:37 -0500

    utils/accelerators.mk: fix dependency of hls working directory

--- utils/accelerators.mk
+++ utils/accelerators.mk
@@ -1,19 +1,20 @@
 
-ACCELERATORS_PATH = $(ESP_ROOT)/accelerators
-ACCELERATORS = $(filter-out common, $(shell ls -d $(ACCELERATORS_PATH)/*/ | awk -F/ '{print $$(NF-1)}'))
-
-ACCELERATORS-clean = $(addsuffix -clean, $(ACCELERATORS))
+ACCELERATORS_PATH      = $(ESP_ROOT)/accelerators
+ACCELERATORS           = $(filter-out common, $(shell ls -d $(ACCELERATORS_PATH)/*/ | awk -F/ '{print $$(NF-1)}'))
+ACCELERATORS-wdir      = $(addsuffix -wdir, $(ACCELERATORS))
+ACCELERATORS-hls       = $(addsuffix -hls, $(ACCELERATORS))
+ACCELERATORS-clean     = $(addsuffix -clean, $(ACCELERATORS))
 ACCELERATORS-distclean = $(addsuffix -distclean, $(ACCELERATORS))
+ACCELERATORS-sim       = $(addsuffix -sim, $(ACCELERATORS))
+ACCELERATORS-plot      = $(addsuffix -plot, $(ACCELERATORS))
 
-ACCELERATORS-sim = $(addsuffix -sim, $(ACCELERATORS))
-
-ACCELERATORS-plot = $(addsuffix -plot, $(ACCELERATORS))
 
 print-available-accelerators:
 	$(QUIET_INFO)echo "Available accelerators: $(ACCELERATORS)"
 
-$(ACCELERATORS):
-	$(QUIET_RUN)mkdir -p $(ACCELERATORS_PATH)/$@/hls-work-$(TECHLIB)
+
+$(ACCELERATORS-wdir):
+	$(QUIET_MKDIR)mkdir -p $(ACCELERATORS_PATH)/$@/hls-work-$(TECHLIB)
 	@cd $(ACCELERATORS_PATH)/$@/hls-work-$(TECHLIB); \
 	if test ! -e project.tcl; then \
 		ln -s ../stratus/project.tcl; \
@@ -21,31 +22,33 @@ $(ACCELERATORS):
 	if test ! -e Makefile; then \
 		ln -s ../stratus/Makefile; \
 	fi;
+
+$(ACCELERATORS-hls): %-hls : %-wdir
 	$(QUIET_MAKE)ACCELERATOR=$@ TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$@/hls-work-$(TECHLIB) memlib | tee $@_memgen.log
 	$(QUIET_INFO)echo "Running HLS for available implementations of $@"
 	$(QUIET_MAKE)ACCELERATOR=$@ TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$@/hls-work-$(TECHLIB) hls_all | tee $@_hls.log
 	$(QUIET_INFO)echo "Installing available implementations for $@ to $(ESP_ROOT)/tech/$(TECHLIB)/acc/$@"
 	$(QUIET_MAKE)ACCELERATOR=$@ TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$@/hls-work-$(TECHLIB) install
 
-$(ACCELERATORS-sim):
+$(ACCELERATORS-sim): %-sim : %-wdir
 	@$(QUIET_RUN)ACCELERATOR=$(@:-sim=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-sim=)/hls-work-$(TECHLIB) sim_all | tee $@_sim.log
 
-$(ACCELERATORS-plot):
+$(ACCELERATORS-plot): %-plot : %-wdir
 	@$(QUIET_RUN)ACCELERATOR=$(@:-plot=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-plot=)/hls-work-$(TECHLIB) plot
 
-$(ACCELERATORS-clean):
+$(ACCELERATORS-clean): %-clean : %-wdir
 	@$(QUIET_CLEAN)ACCELERATOR=$(@:-clean=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-clean=)/hls-work-$(TECHLIB) clean
 
-$(ACCELERATORS-distclean):
+$(ACCELERATORS-distclean): %-distclean : %-wdir
 	@$(QUIET_CLEAN)ACCELERATOR=$(@:-distclean=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-distclean=)/hls-work-$(TECHLIB) distclean
 	@$(RM) $(ESP_ROOT)/tech/$(TECHLIB)/acc/$(@:-distclean=)
 
-.PHONY: print-available-accelerators $(ACCELERATORS) $(ACCELERATORS-sim) $(ACCELERATORS-plot) $(ACCELERATORS-clean) $(ACCELERATORS-distclean)
+.PHONY: print-available-accelerators $(ACCELERATORS-wdir) $(ACCELERATORS-hls) $(ACCELERATORS-sim) $(ACCELERATORS-plot) $(ACCELERATORS-clean) $(ACCELERATORS-distclean)
 
-accelerators: $(ACCELERATORS)
+accelerators: $(ACCELERATORS-hls)
 
-accelerators-clean: $(ACCELERATORS)-clean
+accelerators-clean: $(ACCELERATORS-clean)
 
-accelerators-distclean: $(ACCELERATORS)-distclean
+accelerators-distclean: $(ACCELERATORS-distclean)
 
 .PHONY: accelerators accelerators-clean accelerators-distclean
