ec4d3dbd3248432e5d0078ecdbdb4ea48a731251
Paolo Mantovani <paolo@cs.columbia.edu>
2017-03-31 14:57:33 -0400

    soft/leon3/drivers: add libtest

---
+++ soft/leon3/drivers/test/le.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <test/test.h>
+#include <test/le.h>
+
+#ifdef __sparc__
+
+size_t lefread(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	/**
+	 * fread()  and  fwrite()  return  the number of items successfully read
+	 * or written (i.e., not the number of characters).  If an error occurs,
+	 * or the end-of-file is reached, the return value is a short item count
+	 * (or zero).
+	 *
+	 * fread() does not distinguish between end-of-file and error, and
+	 * callers must use feof(3) and ferror(3) to determine which occurred.
+	*/
+	size_t n;
+	unsigned char *p;
+	unsigned char *buf;
+	int i, j;
+
+	if (size == 1)
+		return fread(ptr, size, nmemb, stream);
+
+	n = fread(ptr, size, nmemb, stream);
+	if (n == 0)
+		return n;
+
+	p = ptr;
+	buf = malloc(size);
+	if (!buf || !p)
+		return 0;
+
+	for (i = 0; i < nmemb; i++) {
+		memcpy((void *) buf, (void *) p + size*i, size);
+		for (j = 0; j < size; j++)
+			p[size*i + j] = buf[size-j-1];
+	}
+
+	free(buf);
+	return n;
+}
+
+size_t lefwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	/**
+	 * fread()  and  fwrite()  return  the number of items successfully read
+	 * or written (i.e., not the number of characters).  If an error occurs,
+	 * or the end-of-file is reached, the return value is a short item count
+	 * (or zero).
+	 *
+	 * fread() does not distinguish between end-of-file and error, and
+	 * callers must use feof(3) and ferror(3) to determine which occurred.
+	*/
+	const unsigned char *p;
+	unsigned char *buf;
+	size_t n;
+	int i, j;
+
+	if (size == 1)
+		return fwrite(ptr, size, nmemb, stream);
+
+	p = ptr;
+	buf = malloc(nmemb * size);
+	if (!buf)
+		return 0;
+
+	for (i = 0; i < nmemb; i++)
+		for (j = 0; j < size; j++)
+			buf[size * i + j] = p[size * (i + 1) - j - 1];
+
+	n = fwrite(buf, size, nmemb, stream);
+	free(buf);
+
+	return n;
+}
+
+#else
+
+size_t lefread(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	return fread(ptr, size, nmemb, stream);
+}
+size_t lefwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	return fwrite(ptr, size, nmemb, stream);
+}
+
+#endif /* __sparc__ */
+
+void le_read_elem(void *dest, size_t size_elem, size_t n_elems, FILE *fp, const char *path)
+{
+	size_t n;
+
+	n = lefread(dest, size_elem, n_elems, fp);
+	if (n != n_elems) {
+		die("Unable to read phase history data from %s "
+			"(read %zu elements instead of %lu).\n",
+			path, n, n_elems);
+	}
+}
---
+++ soft/leon3/drivers/test/test.c
@@ -0,0 +1,275 @@
+#define _GNU_SOURCE /* asprintf */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <test/test.h>
+#include <test/time.h>
+
+static void dev_open(struct test_info *info)
+{
+	info->fd = open(info->devname, O_RDWR, 0);
+	if (info->fd < 0) {
+		perror(__func__);
+		exit(1);
+	}
+}
+
+static void __dev_init(struct test_info *info, bool pr)
+{
+	if (!info->fd) {
+		if (pr) {
+			printf("Opening %s... ", info->devname);
+			fflush(NULL);
+		}
+		dev_open(info);
+	}
+
+	if (pr) {
+		printf("Allocating buffers... ");
+		fflush(NULL);
+	}
+	info->alloc_buf(info);
+
+	if (pr) {
+		printf("Allocating hardware buffer... ");
+		fflush(NULL);
+	}
+	info->alloc_contig(info);
+
+	if (pr) {
+		printf("Initializing buffers... ");
+		fflush(NULL);
+	}
+	info->init_bufs(info);
+
+	if (info->to_sc_fixed) {
+		if (pr) {
+			printf("Converting to fixed point... ");
+			fflush(NULL);
+		}
+		info->to_sc_fixed(info);
+	}
+
+	if (info->set_access)
+		info->set_access(info);
+	info->esp->contig = contig_to_khandle(info->contig);
+	info->esp->run = true;
+}
+
+static void dev_init(struct test_info *info)
+{
+	__dev_init(info, false);
+}
+
+static void pr_file(const char *filename, const char *pfx, const char *description, const struct timespec *ts)
+{
+	FILE *fp;
+
+	fp = fopen(filename, "w+");
+	if (fp == NULL) {
+		perror(__func__);
+		exit(1);
+	}
+	fprintf(fp, "%s: %llu (%s)\n", pfx, getformattedtime(ts), description);
+	if (fclose(fp)) {
+		perror(__func__);
+		exit(1);
+	}
+}
+
+static char *str_to_upper(const char *str)
+{
+	char *new;
+	char *p;
+
+	new = strdup(str);
+	if (new == NULL) {
+		perror(__func__);
+		exit(1);
+	}
+	p = new;
+	while (*p)
+		*p++ = toupper(*p);
+	return new;
+}
+
+static int cmd_test(struct test_info *info)
+{
+	struct timespec ts_start, ts_end;
+	struct timespec th_start, th_end;
+	unsigned long long sw_ns;
+	unsigned long long hw_ns;
+	int rc;
+
+	__dev_init(info, true);
+
+	if (info->comp) {
+		printf("Running software\n");
+		gettime(&ts_start);
+		info->comp(info);
+		gettime(&ts_end);
+		printf("Software done!\n\n");
+	}
+
+	printf("Running hardware\n");
+	gettime(&th_start);
+	rc = ioctl(info->fd, info->cm, info->esp);
+	gettime(&th_end);
+	if (rc < 0)
+		die_errno("IOCTL on %s failed", info->devname);
+	printf("HW done");
+	if (info->to_float) {
+		printf("... Converting to float");
+		fflush(NULL);
+		info->to_float(info);
+	}
+	printf("\n\n");
+
+	hw_ns = ts_subtract(&th_start, &th_end);
+	sw_ns = ts_subtract(&ts_start, &ts_end);
+	printf("hw exec time: %llu ns\n", hw_ns);
+	printf("sw exec time: %llu ns\n", sw_ns);
+
+	if (!info->diff_ok(info))
+		return 1;
+	else
+		printf("OK: Software and Hardware match\n");
+	return 0;
+}
+
+static int cmd_hw(struct test_info *info)
+{
+	struct timespec ts_start, ts_end;
+	struct timespec th_start, th_end;
+	char *uppername;
+	int rc;
+
+	gettime(&ts_start);
+	dev_init(info);
+	gettime(&th_start);
+	rc = ioctl(info->fd, info->cm, info->esp);
+	gettime(&th_end);
+	if (rc < 0)
+		die_errno("IOCTL on %s failed", info->devname);
+	gettime(&ts_end);
+
+	uppername = str_to_upper(info->name);
+	printf("%s init: %llu\n", uppername, getformattedtime(&ts_start));
+	printf("%s hw-init: %llu\n", uppername, getformattedtime(&th_start));
+	printf("%s hw-end: %llu\n", uppername, getformattedtime(&th_end));
+	printf("%s end: %llu\n", uppername, getformattedtime(&ts_end));
+	free(uppername);
+	return 0;
+}
+
+static int cmd_run(struct test_info *info)
+{
+	struct timespec th_start;
+	char *filename;
+	char *uppername;
+	int rc;
+
+	dev_open(info);
+
+	gettime(&th_start);
+	rc = ioctl(info->fd, ESP_IOC_RUN);
+	if (rc < 0)
+		die_errno("ESP_IOC_RUN on %s failed", info->devname);
+	if (asprintf(&filename, "%s-start.txt", info->name) < 0) {
+		perror("asprintf");
+		exit(1);
+	}
+	uppername = str_to_upper(info->name);
+	pr_file(filename, uppername, "hardware-start", &th_start);
+	free(uppername);
+	free(filename);
+	return 0;
+}
+
+static int cmd_config(struct test_info *info)
+{
+	struct timespec th_end;
+	char *filename;
+	char *uppername;
+	int rc;
+
+	dev_init(info);
+	info->esp->run = false;
+	rc = ioctl(info->fd, info->cm, info->esp);
+	gettime(&th_end);
+	if (rc < 0)
+		die_errno("IOCTL on %s failed", info->devname);
+	if (asprintf(&filename, "%s-end.txt", info->name) < 0) {
+		perror("asprintf");
+		exit(1);
+	}
+	uppername = str_to_upper(info->name);
+	pr_file(filename, uppername, "hardware-end", &th_end);
+	free(uppername);
+	free(filename);
+	return 0;
+}
+
+int test_main(struct test_info *info, const char *cmd)
+{
+	info->cmd = cmd;
+
+	if (!strcmp(cmd, "config"))
+		return cmd_config(info);
+	else if (!strcmp(cmd, "test"))
+		return cmd_test(info);
+	else if (!strcmp(cmd, "run"))
+		return cmd_run(info);
+	else if (!strcmp(cmd, "hw"))
+		return cmd_hw(info);
+	fprintf(stderr, "unknown cmd '%s'\n", cmd);
+	return 1;
+}
+
+static int err_vfmt(const char *prefix, const char *fmt, va_list ap)
+{
+        char err[2048];
+
+        vsnprintf(err, sizeof(err), fmt, ap);
+        return fprintf(stderr, "%s%s\n", prefix, err);
+}
+
+static void NORETURN die_vfmt(const char *fmt, va_list ap)
+{
+        err_vfmt("fatal: ", fmt, ap);
+        exit(EXIT_FAILURE);
+}
+
+void NORETURN die(const char *fmt, ...)
+{
+        va_list ap;
+
+        va_start(ap, fmt);
+        die_vfmt(fmt, ap);
+        va_end(ap);
+}
+
+static void NORETURN errno_vfmt(const char *fmt, va_list ap)
+{
+        char err[1024];
+
+        snprintf(err, sizeof(err), "%s: %s", fmt, strerror(errno));
+        die_vfmt(err, ap);
+}
+
+void NORETURN die_errno(const char *fmt, ...)
+{
+        va_list ap;
+
+        va_start(ap, fmt);
+        errno_vfmt(fmt, ap);
+        va_end(ap);
+}
