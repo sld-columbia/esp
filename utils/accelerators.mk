5e9786a477bd2a90d7b4df92ab119312040d7e2f
Paolo Mantovani <paolo@cs.columbia.edu>
2017-03-31 15:04:54 -0400

    utils/accelerators.mk: add targets for drivers

--- utils/accelerators.mk
+++ utils/accelerators.mk
@@ -1,12 +1,18 @@
 
-ACCELERATORS_PATH      = $(ESP_ROOT)/accelerators
-ACCELERATORS           = $(filter-out common, $(shell ls -d $(ACCELERATORS_PATH)/*/ | awk -F/ '{print $$(NF-1)}'))
-ACCELERATORS-wdir      = $(addsuffix -wdir, $(ACCELERATORS))
-ACCELERATORS-hls       = $(addsuffix -hls, $(ACCELERATORS))
-ACCELERATORS-clean     = $(addsuffix -clean, $(ACCELERATORS))
-ACCELERATORS-distclean = $(addsuffix -distclean, $(ACCELERATORS))
-ACCELERATORS-sim       = $(addsuffix -sim, $(ACCELERATORS))
-ACCELERATORS-plot      = $(addsuffix -plot, $(ACCELERATORS))
+ACCELERATORS_PATH		= $(ESP_ROOT)/accelerators
+ACCELERATORS			= $(filter-out common, $(shell ls -d $(ACCELERATORS_PATH)/*/ | awk -F/ '{print $$(NF-1)}'))
+ACCELERATORS-wdir		= $(addsuffix -wdir, $(ACCELERATORS))
+ACCELERATORS-hls		= $(addsuffix -hls, $(ACCELERATORS))
+ACCELERATORS-clean		= $(addsuffix -clean, $(ACCELERATORS))
+ACCELERATORS-distclean		= $(addsuffix -distclean, $(ACCELERATORS))
+ACCELERATORS-sim		= $(addsuffix -sim, $(ACCELERATORS))
+ACCELERATORS-plot		= $(addsuffix -plot, $(ACCELERATORS))
+ACCELERATORS-driver		= $(addsuffix -driver, $(ACCELERATORS))
+ACCELERATORS-driver-clean	= $(addsuffix -driver-clean, $(ACCELERATORS))
+ACCELERATORS-app		= $(addsuffix -app, $(ACCELERATORS))
+ACCELERATORS-app-clean		= $(addsuffix -app-clean, $(ACCELERATORS))
+ACCELERATORS-barec		= $(addsuffix -barec, $(ACCELERATORS))
+ACCELERATORS-barec-clean	= $(addsuffix -barec-clean, $(ACCELERATORS))
 
 
 print-available-accelerators:
@@ -33,17 +39,17 @@ $(ACCELERATORS-hls): %-hls : %-wdir
 	@echo "$(@:-hls=)" >> $(ESP_ROOT)/tech/$(TECHLIB)/acc/installed.log
 
 $(ACCELERATORS-sim): %-sim : %-wdir
-	@$(QUIET_RUN)ACCELERATOR=$(@:-sim=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-sim=)/hls-work-$(TECHLIB) sim_all | tee $(@:-sim=)_sim.log
+	$(QUIET_RUN)ACCELERATOR=$(@:-sim=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-sim=)/hls-work-$(TECHLIB) sim_all | tee $(@:-sim=)_sim.log
 
 $(ACCELERATORS-plot): %-plot : %-wdir
-	@$(QUIET_RUN)ACCELERATOR=$(@:-plot=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-plot=)/hls-work-$(TECHLIB) plot
+	$(QUIET_RUN)ACCELERATOR=$(@:-plot=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-plot=)/hls-work-$(TECHLIB) plot
 
 $(ACCELERATORS-clean): %-clean : %-wdir
-	@$(QUIET_CLEAN)ACCELERATOR=$(@:-clean=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-clean=)/hls-work-$(TECHLIB) clean
+	$(QUIET_CLEAN)ACCELERATOR=$(@:-clean=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-clean=)/hls-work-$(TECHLIB) clean
 	@$(RM) $(@:-clean=)*.log
 
 $(ACCELERATORS-distclean): %-distclean : %-wdir
-	@$(QUIET_CLEAN)ACCELERATOR=$(@:-distclean=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-distclean=)/hls-work-$(TECHLIB) distclean
+	$(QUIET_CLEAN)ACCELERATOR=$(@:-distclean=) TECH=$(TECHLIB) ESP_ROOT=$(ESP_ROOT) make -C $(ACCELERATORS_PATH)/$(@:-distclean=)/hls-work-$(TECHLIB) distclean
 	@$(RM) $(@:-distclean=)*.log
 	@sed -i '/$(@:-distclean=)/d' $(ESP_ROOT)/tech/$(TECHLIB)/acc/installed.log
 
@@ -73,3 +79,69 @@ accelerators-clean: $(ACCELERATORS-clean)
 accelerators-distclean: $(ACCELERATORS-distclean)
 
 .PHONY: sldgen-clean sldgen-distclean accelerators accelerators-clean accelerators-distclean
+
+$(ACCELERATORS-driver): sysroot linux-build/vmlinux
+	@if test -e $(DRIVERS)/$(@:-driver=)/linux/$(@:-driver=).c; then \
+		echo '   ' MAKE $@; \
+		ARCH=sparc CROSS_COMPILE=sparc-leon3-linux- KSRC=$(PWD)/linux-build $(MAKE) ESP_CORE_PATH=$(ESP_CORE_PATH) -C $(DRIVERS)/$(@:-driver=)/linux; \
+		if test -e $(DRIVERS)/$(@:-driver=)/linux/$(@:-driver=).ko; then \
+			echo '   ' CP $@; cp $(DRIVERS)/$(@:-driver=)/linux/$(@:-driver=).ko sysroot/opt/drivers/; \
+		else \
+			echo '   ' WARNING $@ compilation failed!; \
+		fi; \
+	else \
+		echo '   ' WARNING $@ not found!; \
+	fi;
+
+$(ACCELERATORS-driver-clean):
+	$(QUIET_CLEAN)ARCH=sparc CROSS_COMPILE=sparc-leon3-linux- KSRC=$(PWD)/linux-build $(MAKE) ESP_CORE_PATH=$(ESP_CORE_PATH) -C $(DRIVERS)/$(@:-driver-clean=)/linux clean
+
+
+$(ACCELERATORS-app): sysroot
+	@if [ `ls -1 $(DRIVERS)/$(@:-app=)/app/*.c 2>/dev/null | wc -l ` -gt 0 ]; then \
+		echo '   ' MAKE $@; \
+		CROSS_COMPILE=sparc-leon3-linux- $(MAKE) -C $(DRIVERS)/$(@:-app=)/app; \
+		if [ `ls -1 $(DRIVERS)/$(@:-app=)/app/*.exe 2>/dev/null | wc -l ` -gt 0 ]; then \
+			echo '   ' CP $@; cp $(DRIVERS)/$(@:-app=)/app/*.exe sysroot/applications/test/; \
+		else \
+			echo '   ' WARNING $@ compilation failed!; \
+		fi; \
+	else \
+		echo '   ' WARNING $@ not found!; \
+	fi;
+
+$(ACCELERATORS-app-clean):
+	$(QUIET_CLEAN)CROSS_COMPILE=sparc-leon3-linux- $(MAKE) -C $(DRIVERS)/$(@:-app-clean=)/app clean
+
+$(ACCELERATORS-barec): barec
+	@if [ `ls -1 $(DRIVERS)/$(@:-barec=)/barec/*.c 2>/dev/null | wc -l ` -gt 0 ]; then \
+		echo '   ' MAKE $@; \
+		CROSS_COMPILE=sparc-elf- $(MAKE) -C $(DRIVERS)/$(@:-barec=)/barec; \
+		if [ `ls -1 $(DRIVERS)/$(@:-barec=)/barec/*.exe 2>/dev/null | wc -l ` -gt 0 ]; then \
+			echo '   ' CP $@; cp $(DRIVERS)/$(@:-barec=)/barec/*.exe barec; \
+		else \
+			echo '   ' WARNING $@ compilation failed!; \
+		fi; \
+	else \
+		echo '   ' WARNING $@ not found!; \
+	fi;
+
+$(ACCELERATORS-barec-clean):
+	$(QUIET_CLEAN)CROSS_COMPILE=sparc-elf- $(MAKE) -C $(DRIVERS)/$(@:-barec-clean=)/barec clean
+
+
+.PHONY: $(ACCELERATORS-driver) $(ACCELERATORS-driver-clean) $(ACCELERATORS-app) $(ACCELERATORS-app-clean) $(ACCELERATORS-barec) $(ACCELERATORS-barec-clean)
+
+accelerators-driver: $(ACCELERATORS-driver)
+
+accelerators-driver-clean: $(ACCELERATORS-driver-clean)
+
+accelerators-app: $(ACCELERATORS-app)
+
+accelerators-app-clean: $(ACCELERATORS-app-clean)
+
+accelerators-barec: $(ACCELERATORS-barec)
+
+accelerators-barec-clean: $(ACCELERATORS-barec-clean)
+
+.PHONY: accelerators-driver accelerators-driver-clean accelerators-app accelerators-app-clean accelerators-barec accelerators-barec-clean
