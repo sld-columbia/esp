8d6a5da99b53ec3d34f6fe1dec62e6144998e8f9
Paolo Mantovani <paolo@cs.columbia.edu>
2017-03-31 14:54:55 -0400

    soft/leon3/drivers: add dvi baremetal and linux drivers

---
+++ soft/leon3/drivers/dvi/barec/draw.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define VRES 600
+#define HRES 800
+
+int main(int argc, char **argv)
+{
+
+	int i, j;
+	volatile unsigned *mem = (unsigned *) 0x80000614;
+
+	i = 0;
+	for (i = 0; i < 600; i++)
+		for (j = 0; j < 200; j++)
+			mem[i * 200 + j] = 0;
+
+	for (i = 0; i < 600; i++)
+		for (j = 0; j < 200; j++) {
+			if (j*4 > 600)
+				mem[i * 200 + j] = 0x0;
+			else if (i < (j*4 - 4) || i > (j*4 + 4))
+				mem[i * 200 + j] = 0x0c0c0c0c;
+			else
+				mem[i * 200 + j] = 0xc4c4c4c4;
+		}
+
+
+	return 0;
+}
