f75b74e19c971d5815b9eae2c0832c509e101d45
Davide Giri <davide_giri@cs.columbia.edu>
2018-02-23 18:00:12 -0500

    Add the systest that runs all the test suite for the caches.

--- socs/xilinx-vc707-xc7vx485t/systest.c
+++ socs/xilinx-vc707-xc7vx485t/systest.c
@@ -1,16 +1,71 @@
 #include <stdio.h>
+#include "defines.h"
 
-int main(int argc, char **argv)
+/* Test suite guidelines:
+ * - All function names containing the word 'report' set the related flag in an array, that will be
+ *   parsed at the end of the execution to verify the correctness of the execution. Those the
+ *   additionally contain the word 'fail' will report in the same way execution errors.
+ * - Assuming words of 32 bits.
+ */
+
+volatile int sync_start[MAX_N_CPU] = {0, 0, 0, 1};
+volatile int sync_leon3_test[MAX_N_CPU] = {0, 0, 0, 0};
+
+int main()
 {
-    flush();
+    int * irqmp_ptr = (int *) 0x80000200;
+    int ncpu = (((*(irqmp_ptr + 0x10/4)) >> 28) & 0x0f) + 1;
+    int pid = get_pid();
 
-    printf("Hello ESP!\n");
+    /* Start all CPUs */
+    if (!pid)
+	mptest_start(0x80000200);
 
-    l2_cache_test();
-    read_report();
+    /* Initialize the report arrays */
+    /* Report the start of the whole test */
+    psync(sync_start, pid, ncpu);
+    if (!pid) {
+	report_init();
+	report_test(TEST_START);
+	data_structures_setup();
+    }
 
-    leon3_test(0, 0x80000200, 1);
-    read_report();
+    psync(sync_leon3_test, pid, ncpu);
+
+    /* Execute part of the original grlib tests.  */
+    report_test(TEST_LEON3);
+    leon3_test(1, 0x80000200, 0);
+
+    /* Completely fill and evict all caches */
+    report_test(TEST_FILL);
+    cache_fill(L2_WAYS, ncpu); // select INT, HALFWORD and BYTE in defines.h
+
+    /* Test false sharing */
+    report_test(TEST_SHARING);
+    false_sharing(SETS, ncpu);
+
+    /* Test some random read and write operations */
+    report_test(TEST_RAND_RW);
+    rand_rw(SETS, ncpu); // select INT, HALFWORD and BYTE in defines.h
+
+    /* Test some random read and write operations */
+    report_test(TEST_LOCK);
+    test_lock(5, ncpu); // select INT, HALFWORD and BYTE in defines.h
+
+    /* Test MESI protocol */
+    report_test(TEST_MESI);
+    if (mesi_test(ncpu, 15))
+    	report_fail(FAIL_MESI);
+
+    /* Test MP and turn off all CPUs but one */
+    report_test(TEST_MP_END);
+    mptest_end(0x80000200);
+
+    /* Report the end of the whole test */
+    report_test(TEST_END);
+
+    /* Printout results */
+    report_parse(ncpu);
 
     return 0;
 }
