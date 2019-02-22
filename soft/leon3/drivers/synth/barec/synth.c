d20a4d828a0b00b8db4fbd0a29636e5083fc6968
Paolo Mantovani <paolo@cs.columbia.edu>
2019-02-22 15:20:48 -0500

    add bare-metal stub for synthetic accelerator

---
+++ soft/leon3/drivers/synth/barec/synth.c
@@ -0,0 +1,126 @@
+/**
+ * Baremetal device driver for SYNTH
+ *
+ * Select Scatter-Gather in ESP configuration
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <esp_accelerator.h>
+#include <esp_probe.h>
+
+#define SLD_SYNTH   0x012
+#define DEV_NAME "synth"
+
+// Accelerator-specific buffe size
+#define SYNTH_BUF_SIZE (1024 * sizeof(unsigned))
+
+/* Size of the contiguous chunks for scatter/gather */
+#define CHUNK_SHIFT 7
+#define CHUNK_SIZE BIT(CHUNK_SHIFT)
+#define NCHUNK ((SYNTH_BUF_SIZE % CHUNK_SIZE == 0) ?			\
+			(SYNTH_BUF_SIZE / CHUNK_SIZE) :			\
+			(SYNTH_BUF_SIZE / CHUNK_SIZE) + 1)
+
+// User defined registers
+
+
+
+int main(int argc, char * argv[])
+{
+	int n;
+	int ndev;
+	struct esp_device *espdevs = NULL;
+	unsigned coherence;
+
+	ndev = probe(&espdevs, SLD_SYNTH, DEV_NAME);
+	if (!ndev) {
+		fprintf(stderr, "Error: %s device not found!\n", DEV_NAME);
+		exit(EXIT_FAILURE);
+	}
+
+	for (n = 0; n < ndev; n++) {
+		for (coherence = ACC_COH_NONE; coherence <= ACC_COH_FULL; coherence++) {
+			struct esp_device *dev = &espdevs[n];
+			unsigned done;
+			int i;
+			unsigned **ptable;
+			unsigned *mem;
+			int scatter_gather = 1;
+
+			printf("******************** %s.%d ********************\n", DEV_NAME, n);
+
+			// Check if scatter-gather DMA is disabled
+			if (ioread32(dev, PT_NCHUNK_MAX_REG) == 0) {
+				printf("  -> scatter-gather DMA is disabled; revert to contiguous buffer.\n");
+				scatter_gather = 0;
+			} else {
+				printf("  -> scatter-gather DMA is enabled.\n");
+			}
+
+			if (scatter_gather)
+				if (ioread32(dev, PT_NCHUNK_MAX_REG) < NCHUNK) {
+					fprintf(stderr, "  Trying to allocate %lu chunks on %d TLB available entries\n",
+						NCHUNK, ioread32(dev, PT_NCHUNK_MAX_REG));
+					break;
+				}
+
+			// Allocate memory (will be contigous anyway in baremetal)
+			mem = aligned_malloc(SYNTH_BUF_SIZE);
+			printf("  memory buffer base-address = %p\n", mem);
+
+			if (scatter_gather) {
+				//Alocate and populate page table
+				ptable = aligned_malloc(NCHUNK * sizeof(unsigned *));
+				for (i = 0; i < NCHUNK; i++)
+					ptable[i] = (unsigned *) &mem[i * (CHUNK_SIZE / sizeof(unsigned))];
+				printf("  ptable = %p\n", ptable);
+				printf("  nchunk = %lu\n", NCHUNK);
+			}
+
+			// Initialize input: write floating point hex values (simpler to debug)
+
+			// Configure device
+			iowrite32(dev, SELECT_REG, ioread32(dev, DEVID_REG));
+			iowrite32(dev, COHERENCE_REG, coherence);
+
+			if (scatter_gather) {
+				iowrite32(dev, PT_ADDRESS_REG, (unsigned) ptable);
+				iowrite32(dev, PT_NCHUNK_REG, NCHUNK);
+				iowrite32(dev, PT_SHIFT_REG, CHUNK_SHIFT);
+				iowrite32(dev, SRC_OFFSET_REG, 0);
+				iowrite32(dev, DST_OFFSET_REG, 0); // Sort runs in place
+			} else {
+				iowrite32(dev, SRC_OFFSET_REG, (unsigned) mem);
+				iowrite32(dev, DST_OFFSET_REG, (unsigned) mem); // Sort runs in place
+			}
+
+			// Accelerator-specific registers
+
+			// Flush for non-coherent DMA
+			esp_flush(coherence);
+
+			// Start accelerator
+			printf("  Start..\n");
+			iowrite32(dev, CMD_REG, CMD_MASK_START);
+
+			done = 0;
+			while (!done) {
+				done = ioread32(dev, STATUS_REG);
+				done &= STATUS_MASK_DONE;
+			}
+			iowrite32(dev, CMD_REG, 0x0);
+			printf("  Done\n");
+
+			// Validation
+
+			if (scatter_gather)
+				aligned_free(ptable);
+			aligned_free(mem);
+
+			printf("**************************************************\n\n");
+		}
+	}
+	return 0;
+}
+
