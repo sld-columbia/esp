8f68517928150a143f4cf4c54e4e72efe2c542ec
Paolo Mantovani <paolo@cs.columbia.edu>
2019-08-27 12:50:13 -0400

    soft: improve dummy test application

--- soft/leon3/drivers/dummy/app/dummy.c
+++ soft/leon3/drivers/dummy/app/dummy.c
@@ -12,8 +12,12 @@
 #include <dummy.h>
 
 #define TOKENS 512
-#define BATCH 2
-#define SIZE (2 * BATCH * TOKENS)
+#define BATCH 256
+
+typedef unsigned long long u64;
+
+const unsigned out_offset = BATCH * TOKENS * sizeof(u64);
+const unsigned size = 3 * out_offset;
 
 struct accelerator_thread_info {
 	int fd;
@@ -54,86 +58,184 @@ static void prepare_dummy(int *fd, contig_handle_t *mem, accelerator_thread_info
 		desc->esp.run = true;
 		desc->esp.coherence = ACC_COH_NONE;
 		desc->tokens = TOKENS;
-		if (id != 2)
-			desc->batch = 1;
-		else
+		switch(id) {
+		case 0 :
+			desc->batch = BATCH / 2;
+			desc->src_offset = 0;
+			desc->dst_offset = out_offset;
+			break;
+		case 1 :
+			desc->batch = BATCH / 2;
+			desc->src_offset = out_offset / 2;
+			desc->dst_offset = 3 * out_offset / 2;
+			break;
+		default :
 			desc->batch = BATCH;
+			desc->src_offset = out_offset;
+			desc->dst_offset = 2 * out_offset;
+			break;
+		}
+
 		info->fd = *fd;
 		info->desc = *desc;
 }
+
+static void init_buffer(contig_handle_t *hwbuf)
+{
+	int i, j;
+	for (j = 0; j < BATCH; j++)
+		for (i = 0; i < TOKENS; i++)
+			contig_write64(0xFEED0BAC00000000L | (u64) i, *hwbuf, (j * TOKENS + i) * sizeof(u64));
+
+	for (i = 0; i < 2 * BATCH * TOKENS; i++)
+		contig_write64(0xFFFFFFFFFFFFFFFFL, *hwbuf, out_offset + i * sizeof(u64));
+}
+
+static void validate_buffer(contig_handle_t *hwbuf)
+{
+	int i, j;
+	int err = 0;
+
+	for (j = 0; j < BATCH; j++)
+		for (i = 0; i < TOKENS; i++) {
+			u64 token = contig_read64(*hwbuf, 2 * out_offset + (j * TOKENS + i) * sizeof(u64));
+			if (token != (0xFEED0BAC00000000L | (u64) i)) {
+				/* Only print first few errors to debug */
+				if (err < 8)
+					printf("      !! %d.%d: 0x%016llx !!\n", j, i, token);
+				err++;
+			}
+		}
+
+	if (!err)
+		printf("      + Test PASSED\n");
+	else
+		printf("      + Test FAILED\n");
+}
+
+static void print_time_info(accelerator_thread_info_t *info, unsigned long long hw_ns, int nthreads)
+{
+	int i;
+	for (i = 0; i < nthreads; i++)
+		printf("      * ACC.%d time: %llu ns\n", i, info[i].hw_ns);
+	printf("      Test time: %llu ns\n", hw_ns);
+}
+
 
 int main(int argc, char *argv[])
 {
 	int i;
 	int rc = 0;
-	int err;
 	int fd[3];
 	contig_handle_t hwbuf;
 	char device_name[60];
 
-
 	struct timespec th_start;
 	struct timespec th_end;
-	unsigned long long hw_ns = 0;
 
 	pthread_t thread[3];
 	accelerator_thread_info_t info[3];
 	struct dummy_access desc[3];
 
+	void *rp;
+
 	printf("=== Point-to-point test w/ dummy accelerator ===\n");
 
 	printf("  - Config: {tokens = %d, batch = %d}\n", TOKENS, BATCH);
 
-	printf("  - Allocate %lu B\n", SIZE * sizeof(uint64_t));
-	contig_alloc(SIZE * sizeof(uint64_t), &hwbuf);
+	printf("  - Allocate %u B\n", size);
+	contig_alloc(size, &hwbuf);
 
-	printf("  - Initialize %d double words at offset 0\n", BATCH * TOKENS);
-	for (i = 0; i < BATCH * TOKENS; i++)
-		contig_write64(0xFEED0BACL | (uint64_t) i, hwbuf, i * sizeof(uint64_t));
+	printf("  - Initialize input and clear output\n");
+	init_buffer(&hwbuf);
 
+	printf("\n");
+	printf("     * Virtual memory layout *\n");
+        printf("                  |_______________|\n");
+        printf("    ACC0 src -->  |               |\n");
+        printf("                  |    batch 0    |\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                         ...\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                  | batch B/2 - 1 |\n");
+        printf("                  |_______________|\n");
+        printf("    ACC1 src -->  |               |\n");
+        printf("                  |   batch B/2   |\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                         ...\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                  |    batch B    |\n");
+        printf("                  |_______________|\n");
+        printf("    ACC2 src -->  |               | <-- ACC0 dst\n");
+        printf("                  |    batch 0    |\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                         ...\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                  | batch B/2 - 1 |\n");
+        printf("                  |_______________|\n");
+        printf("                  |               | <-- ACC1 dst\n");
+        printf("                  |   batch B/2   |\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                         ...\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                  |    batch B    |\n");
+        printf("                  |_______________|\n");
+        printf("    ACC2 dst -->  |               |\n");
+        printf("                  |    batch 0    |\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                         ...\n");
+        printf("                  |_______________|\n");
+        printf("                  |               |\n");
+        printf("                  |    batch B    |\n");
+        printf("                  |_______________|\n");
+	printf("\n");
+	printf("\n");
+
+	/* Simple DMA */
+	printf("  --> Start simple DMA test\n");
 	printf("  - Configure devices\n");
 	for (i = 0; i < 3; i++) {
 		sprintf(device_name, "/dev/dummy.%d", i);
 		prepare_dummy(&fd[i], &hwbuf, &info[i], &desc[i], device_name, i);
 	}
-
-	printf("  --> Start simple DMA test\n");
 	printf("      MEM ==> ACC.0 ==> MEM\n");
 	printf("      MEM ==> ACC.1 ==> MEM\n");
 	printf("      MEM ==> ACC.2 ==> MEM\n");
 
 	gettime(&th_start);
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 2; i++) {
 		rc = pthread_create(&thread[i], NULL, accelerator_thread, (void*) &info[i]);
 		if(rc != 0) {
 			perror("pthread_create");
 			goto free_and_exit;
 		}
+	}
+	for (i = 0; i < 2; i++) {
 		rc = pthread_join(thread[i], NULL);
 		if(rc != 0) {
 			perror("pthread_join");
 			goto free_and_exit;
 		}
 	}
+	rp = accelerator_thread((void*) &info[i]);
+	if(rp != NULL) {
+		perror("accelerator_thread");
+		goto free_and_exit;
+	}
 	gettime(&th_end);
 
-	err = 0;
-	for (i = 0; i < BATCH * TOKENS; i++) {
-		uint64_t token = contig_read64(hwbuf, (BATCH * TOKENS + i) * sizeof(uint64_t));
-		if (token != (0xFEED0BACL | (uint64_t) i))
-			err++;
-	}
+	validate_buffer(&hwbuf);
 
-	if (!err)
-		printf("      + Test PASSED\n");
-	else
-		printf("      + Test FAILED\n");
-
-
-	for (i = 0; i < 3; i++)
-		printf("      * ACC.%d time: %llu ns\n", i, info[i].hw_ns);
-	hw_ns = ts_subtract(&th_start, &th_end);
-	printf("      Test time: %llu ns\n", hw_ns);
+	print_time_info(info, ts_subtract(&th_start, &th_end), 3);
 
 free_and_exit:
 	contig_free(hwbuf);
