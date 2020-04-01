f41a17b652c374aed561e7254fb1e1aab5c1921b
Paolo Mantovani <paolo@cs.columbia.edu>
2020-04-01 17:16:27 -0400

    Makefile: always check that Xilinx Vivado environment is setup properly

--- utils/design.mk
+++ utils/design.mk
@@ -11,14 +11,14 @@ ifeq ("$(TECHLIB)","")
 $(error technology library not specified)
 endif
 
+ifeq ("$(XILINX_VIVADO)","")
+$(error XILINX_VIVADO path not specified)
+endif
+
 ifneq ($(findstring profpga, $(BOARD)),)
 
 ifeq ("$(PROFPGA)","")
 $(error proFPGA path not specified)
-endif
-
-ifeq ("$(XILINX_VIVADO)","")
-$(error XILINX_VIVADO path not specified)
 endif
 
 PROFPGA_REQUIRED_VER = proFPGA-2019A-SP4
