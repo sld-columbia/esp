c13c1dab4855e6e0ef6abe50f8d49134032c5cf1
Paolo Mantovani <paolo@cs.columbia.edu>
2021-01-11 15:18:07 -0500

    Add definition and link to generated RTL for Harvard U. Systolic Array and NLP accelerators

--- tools/socgen/thirdparty.py
+++ tools/socgen/thirdparty.py
@@ -12,9 +12,17 @@ THIRDPARTY_COMPATIBLE = dict()
 THIRDPARTY_IRQ_TYPE = dict() # IRQ line types: 0 (edge-sensitive), 1
                              # (level-sensitive)
 
-# NVDLA                             
+# NVDLA
 THIRDPARTY_COMPATIBLE["nv_nvdla"] = "nv_small"
 THIRDPARTY_IRQ_TYPE["nv_nvdla"]   = "1"
+
+# SYSARRAY
+THIRDPARTY_COMPATIBLE["hu_sysarray"] = "hu_sysarray"
+THIRDPARTY_IRQ_TYPE["hu_sysarray"]   = "0"
+
+# NLP
+THIRDPARTY_COMPATIBLE["hu_nlp"] = "hu_nlp"
+THIRDPARTY_IRQ_TYPE["hu_nlp"]   = "0"
 
 #
 ######
