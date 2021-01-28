15c6b56d7ded2d80ce5bbd34331092d5cba223ac
Pei Liu <pl2748@columbia.edu>
2021-01-28 09:39:37 -0500

    add MRI-Q accelerator designed with the Stratus HLS flow

---
+++ accelerators/stratus_hls/mriq_stratus/common/sw_exec.h
@@ -0,0 +1,144 @@
+#include <stdlib.h> /* for malloc */
+#include <time.h>
+#include <math.h>
+
+#define PIx2 6.2831853071795864769253f
+
+void sw_comp(int batch_size_k, int num_batch_k, 
+		    int batch_size_x, int num_batch_x,
+		    float *kx, float *ky, float *kz,
+		    float *x, float *y, float *z,
+		    float *phiR, float *phiI,
+		    float *out)
+{
+  float expArg;
+  float cosArg;
+  float sinArg;
+  float phiMag;
+  int indexK, indexX;
+
+  int numK = batch_size_k * num_batch_k;
+
+  for (int b = 0; b < num_batch_x; b++) {
+   
+       int base = b * batch_size_x;
+
+       for (indexX = 0; indexX < batch_size_x; indexX++) {
+           // Sum the contributions to this point over all frequencies
+           float Qracc = 0.0f;
+	   float Qiacc = 0.0f;
+
+
+	   for (indexK = 0; indexK < numK; indexK++) {
+
+	     phiMag = phiR[indexK]*phiR[indexK] + phiI[indexK]*phiI[indexK];
+
+	     expArg = PIx2 * (kx[indexK] * x[base + indexX] +
+			      ky[indexK] * y[base + indexX] +
+			      kz[indexK] * z[base + indexX]);
+	     cosArg = cosf(expArg);
+	     sinArg = sinf(expArg);
+
+	     Qracc += phiMag * cosArg;
+	     Qiacc += phiMag * sinArg;
+	     
+
+	   }
+
+	   out[base * 2 + indexX] = Qracc;
+	   out[base * 2 + indexX + batch_size_x] = Qiacc;
+
+       }
+  }
+}
+
+
+unsigned long long sw_exec(float *gold, float *in, 
+			   int32_t batch_size_x, int32_t num_batch_x,
+			   int32_t batch_size_k, int32_t num_batch_k)
+{
+
+int numX, numK;
+  float *kx, *ky, *kz, *x, *y, *z, *phiR, *phiI;
+  
+  numX = batch_size_x * num_batch_x;
+  numK = batch_size_k * num_batch_k;
+
+  kx = malloc(numK * sizeof(float));
+  ky = malloc(numK * sizeof(float));
+  kz = malloc(numK * sizeof(float));
+  phiR = malloc(numK * sizeof(float));
+  phiI = malloc(numK * sizeof(float));
+
+
+  x = malloc(numX * sizeof(float));
+  y = malloc(numX * sizeof(float));  
+  z = malloc(numX * sizeof(float));
+
+
+  for (int r = 0; r < num_batch_k; r++) {
+    for (int i = 0; i < batch_size_k; i++) {
+      int idx = r * batch_size_k + i;
+      kx[idx] = in[5 * r * batch_size_k + i];
+      ky[idx] = in[(5 * r + 1) * batch_size_k + i];
+      kz[idx] = in[(5 * r + 2) * batch_size_k  + i];
+      phiR[idx] = in[(5 * r+3) * batch_size_k  + i];
+      phiI[idx] = in[(5 * r+4) * batch_size_k  + i];
+
+
+    }
+  }
+
+
+  int base_idx = batch_size_k * num_batch_k * 5;
+
+  for (int r = 0; r < num_batch_x; r++) {
+    for (int i = 0; i < batch_size_x; i++) {
+      int idx = r * batch_size_x + i;
+      x[idx] = in[base_idx + 3 * r * batch_size_x + i];
+      y[idx] = in[base_idx + (3 * r + 1) * batch_size_x + i];
+      z[idx] = in[base_idx + (3 * r + 2) * batch_size_x  + i];
+
+    }
+  }
+
+
+
+  struct timespec ts_start, ts_end;
+  unsigned long long sw_ns;
+
+
+  printf("\n  ** START SW TESTING **\n");
+  gettime(&ts_start);
+
+
+  sw_comp(batch_size_k, num_batch_k, 
+	  batch_size_x, num_batch_x,
+	  kx,\
+	  ky,\
+	  kz,\
+	  x,\
+	  y,\
+	  z,\
+	  phiR,\
+	  phiI,\
+	  gold);
+  gettime(&ts_end);
+
+  sw_ns = ts_subtract(&ts_start, &ts_end);
+
+  printf("  > Test time : %llu (ns)\n", sw_ns);
+  printf("\n  ** DONE **\n\n");
+
+  free(x);
+  free(y);
+  free(z);
+  free(kx);
+  free(ky);
+  free(kz);
+  free(phiR);
+  free(phiI);
+
+  return sw_ns;
+}
+
---
+++ accelerators/stratus_hls/mriq_stratus/hw/src/mriq.hpp
@@ -0,0 +1,153 @@
+// Copyright (c) 2011-2021 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef __MRIQ_HPP__
+#define __MRIQ_HPP__
+
+#include "mriq_conf_info.hpp"
+#include "mriq_debug_info.hpp"
+
+#include "esp_templates.hpp"
+
+#include "mriq_directives.hpp"
+#include "fpdata.hpp"
+#define __round_mask(x, y) ((y)-1)
+#define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
+/* <<--defines-->> */
+
+
+#define DATA_WIDTH FPDATA_S_WL // could be 32 or 24
+#define DMA_SIZE SIZE_WORD
+
+
+class mriq : public esp_accelerator_3P<DMA_WIDTH>
+{
+public:
+    // Constructor
+    SC_HAS_PROCESS(mriq);
+    mriq(const sc_module_name& name)
+    : esp_accelerator_3P<DMA_WIDTH>(name)
+        , cfg("config")
+    {
+        // Signal binding
+        cfg.bind_with(*this);
+
+        // Map arrays to memories
+        /* <<--plm-bind-->> */
+
+        HLS_MAP_plm(plm_Qr_ping, PLM_X_NAME);
+        HLS_MAP_plm(plm_Qi_ping, PLM_X_NAME);
+
+        HLS_MAP_plm(plm_Qr_pong, PLM_X_NAME);
+        HLS_MAP_plm(plm_Qi_pong, PLM_X_NAME);
+
+        HLS_MAP_plm(plm_x_ping, PLM_X_NAME);
+        HLS_MAP_plm(plm_y_ping, PLM_X_NAME);
+        HLS_MAP_plm(plm_z_ping, PLM_X_NAME);
+
+        HLS_MAP_plm(plm_x_pong, PLM_X_NAME);
+        HLS_MAP_plm(plm_y_pong, PLM_X_NAME);
+        HLS_MAP_plm(plm_z_pong, PLM_X_NAME);
+
+
+#if(ARCH==0) // if loading all K-space data into PLM, no need for ping-pong
+
+        HLS_MAP_plm(plm_kx, PLM_K_NAME);
+        HLS_MAP_plm(plm_ky, PLM_K_NAME);
+        HLS_MAP_plm(plm_kz, PLM_K_NAME);
+        HLS_MAP_plm(plm_phiR, PLM_K_NAME);
+        HLS_MAP_plm(plm_phiI, PLM_K_NAME);
+
+#else
+        HLS_MAP_plm(plm_kx_ping, PLM_K_NAME);
+        HLS_MAP_plm(plm_ky_ping, PLM_K_NAME);
+        HLS_MAP_plm(plm_kz_ping, PLM_K_NAME);
+        HLS_MAP_plm(plm_phiR_ping, PLM_K_NAME);
+        HLS_MAP_plm(plm_phiI_ping, PLM_K_NAME);
+
+        HLS_MAP_plm(plm_kx_pong, PLM_K_NAME);
+        HLS_MAP_plm(plm_ky_pong, PLM_K_NAME);
+        HLS_MAP_plm(plm_kz_pong, PLM_K_NAME);
+        HLS_MAP_plm(plm_phiR_pong, PLM_K_NAME);
+        HLS_MAP_plm(plm_phiI_pong, PLM_K_NAME);
+#endif
+
+
+
+    }
+
+    // Processes
+
+    // Load the input data
+    void load_input();
+
+    // Computation
+    void compute_kernel();
+
+    // Store the output data
+    void store_output();
+
+    // Configure mriq
+    esp_config_proc cfg;
+
+    // Functions
+    template<typename T>
+    void dma_read(T array[], uint32_t dma_index, uint32_t dma_length);
+
+    template<typename T>
+    void dma_write(T array[], uint32_t dma_index, uint32_t dma_length);
+
+    void ComputeQ(FPDATA_S x,FPDATA_S y,FPDATA_S z, uint16_t  batch_size_k,
+		  bool pingpong_k, FPDATA_S *sin_table,
+		  FPDATA_L *Qr, FPDATA_L *Qi);
+
+
+    FPDATA_S mySinf(FPDATA_L angle, FPDATA_S *sin_table);
+
+
+
+    // Private local memories
+    // FPDATA_S_WORD means data with smaller values.
+    // For MRI-Q matrix, input and output data range are fixed.
+    FPDATA_S_WORD plm_x_ping[PLM_X_WORD];
+    FPDATA_S_WORD plm_y_ping[PLM_X_WORD];
+    FPDATA_S_WORD plm_z_ping[PLM_X_WORD];
+
+    FPDATA_S_WORD plm_x_pong[PLM_X_WORD];
+    FPDATA_S_WORD plm_y_pong[PLM_X_WORD];
+    FPDATA_S_WORD plm_z_pong[PLM_X_WORD];
+
+
+    FPDATA_L_WORD plm_Qr_ping[PLM_X_WORD];
+    FPDATA_L_WORD plm_Qi_ping[PLM_X_WORD];
+    FPDATA_L_WORD plm_Qr_pong[PLM_X_WORD];
+    FPDATA_L_WORD plm_Qi_pong[PLM_X_WORD];
+
+#if(ARCH==0)
+
+    FPDATA_S_WORD plm_kx[PLM_K_WORD];
+    FPDATA_S_WORD plm_ky[PLM_K_WORD];
+    FPDATA_S_WORD plm_kz[PLM_K_WORD];
+    FPDATA_S_WORD plm_phiR[PLM_K_WORD];
+    FPDATA_S_WORD plm_phiI[PLM_K_WORD];
+#else
+
+    FPDATA_S_WORD plm_kx_ping[PLM_K_WORD];
+    FPDATA_S_WORD plm_ky_ping[PLM_K_WORD];
+    FPDATA_S_WORD plm_kz_ping[PLM_K_WORD];
+    FPDATA_S_WORD plm_phiR_ping[PLM_K_WORD];
+    FPDATA_S_WORD plm_phiI_ping[PLM_K_WORD];
+
+    FPDATA_S_WORD plm_kx_pong[PLM_K_WORD];
+    FPDATA_S_WORD plm_ky_pong[PLM_K_WORD];
+    FPDATA_S_WORD plm_kz_pong[PLM_K_WORD];
+    FPDATA_S_WORD plm_phiR_pong[PLM_K_WORD];
+    FPDATA_S_WORD plm_phiI_pong[PLM_K_WORD];
+
+#endif
+
+
+};
+
+
+#endif /* __MRIQ_HPP__ */
