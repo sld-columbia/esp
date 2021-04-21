48a23ebd8ef45f2f4c47fe634d462fee82d9e291
Paolo Mantovani <paolo@cs.columbia.edu>
2021-04-21 18:36:36 -0400

    Use Xcelium as default simulator for Stratus HLS accelerators

--- accelerators/stratus_hls/nightvision_stratus/hw/hls/project.tcl
+++ accelerators/stratus_hls/nightvision_stratus/hw/hls/project.tcl
@@ -110,7 +110,7 @@ set_attr hls_cc_options "$INCLUDES"
 #
 # Simulation Options
 #
-use_systemc_simulator incisive
+use_systemc_simulator xcelium
 set_attr cc_options "$INCLUDES -DCLOCK_PERIOD=$SIM_CLOCK_PERIOD"
 # enable_waveform_logging -vcd
 set_attr end_of_sim_command "make saySimPassed"
