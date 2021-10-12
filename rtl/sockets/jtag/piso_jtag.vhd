f7642dca8a09e47945300607107cc1254670343f
Gabriele Tombesi <gt2448@columbia.edu>
2021-10-12 14:40:54 -0400

    jtag debug unit: bug fix and round robin over NoC planes

--- rtl/sockets/jtag/piso_jtag.vhd
+++ rtl/sockets/jtag/piso_jtag.vhd
@@ -4,22 +4,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity piso is
+entity piso_jtag is
   generic (
-    sz : integer);
+    sz        : integer;
+    shift_dir : integer := 0);
   port (
     rst      : in  std_logic;
     clk      : in  std_logic;
     clear    : in  std_logic;
     load     : in  std_logic;
     A        : in  std_logic_vector(sz-1 downto 0);
-    B        : out std_logic_vector(sz-1 downto 0);
     shift_en : in  std_logic;
     Y        : out std_logic;
     done     : out std_logic);
-end piso;
+end piso_jtag;
 
-architecture arch of piso is
+architecture arch of piso_jtag is
 
   signal sr     : std_logic_vector(sz downto 0);
   constant ZERO : std_logic_vector(sz-1 downto 0) := (others => '0');
@@ -27,24 +27,38 @@ architecture arch of piso is
 
 begin
 
-  B <= sr(sz downto 1);
 
   process (clk, rst)
   begin
-    -- sr<=(others<='0');
     if rst = '0' then
       sr <= (others => '0');
     elsif clk'event and clk = '1' then
       if clear = '1' then
         sr <= (others => '0');
       elsif load = '1' then
-        sr <= A & '1';
+        if shift_dir = 0 then
+          sr <= A & '1';
+        else
+          sr <= '0' & A;
+        end if;
       elsif shift_en = '1' then
-        sr <= sr(sz-1 downto 0) & '0';
+        if shift_dir = 0 then
+          sr <= sr(sz-1 downto 0) & '0';
+        else
+          sr <= '0' & sr(sz downto 1);
+        end if;
       end if;
     end if;
   end process;
 
-  Y    <= sr(sz);
+  process(sr)
+  begin
+    if shift_dir = 0 then
+      Y <= sr(sz);
+    else
+      Y <= sr(0);
+    end if;
+  end process;
+
   done <= '1' when sr = D else '0';
 end arch;
