f1020b5f4a5d949bddc2c6575423f9230c4d1695
Davide Giri <davide_giri@cs.columbia.edu>
2021-10-19 18:51:21 -0400

    add SLM minimal test app

---
+++ soft/common/apps/baremetal/slm_minimal/slm_minimal.h
@@ -0,0 +1,7 @@
+/* Copyright (c) 2011-2021 Columbia University, System Level Design Group */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef __SLM_MINIMAL_H__
+#define __SLM_MINIMAL_H__
+
+#endif /* __SLM_MINIMAL_H__ */
