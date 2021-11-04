b18d40ba484f31bba9bb19125a32ae02b986c6cd
Davide Giri <davide_giri@cs.columbia.edu>
2021-11-04 03:37:08 -0400

    ahbslv2noc: keep into account HTRANS_BUSY for sending data to ahbs_remote queues

--- rtl/sockets/proxy/ahbslv2noc.vhd
+++ rtl/sockets/proxy/ahbslv2noc.vhd
@@ -422,13 +422,15 @@ begin  -- rtl
         else
           if remote_ahbs_snd_full = '0' then
             hready := '1';
-            remote_ahbs_snd_data_in <= payload_data;
-            remote_ahbs_snd_wrreq <= '1';
-            if selected = '0' or ahbsi.htrans = HTRANS_IDLE then
-              ahbs_next <= idle;
-            elsif sequential = '0' then
-              sample_flits <= '1';
-              ahbs_next <= request_header;
+            if ahbsi.htrans /= HTRANS_BUSY then
+              remote_ahbs_snd_data_in <= payload_data;
+              remote_ahbs_snd_wrreq <= '1';
+              if selected = '0' or ahbsi.htrans = HTRANS_IDLE then
+                ahbs_next <= idle;
+              elsif sequential = '0' then
+                sample_flits <= '1';
+                ahbs_next <= request_header;
+              end if;
             end if;
           end if;
         end if;
