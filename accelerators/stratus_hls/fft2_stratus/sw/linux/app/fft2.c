5a28e96a4094241cc8e9313fd4dd4e0a7205f4a4
Joseph Zuckerman <jzuck@cs.columbia.edu>
2022-01-29 21:54:24 -0500

    :bug: fix bug in fft2_stratus app

--- accelerators/stratus_hls/fft2_stratus/sw/linux/app/fft2.c
+++ accelerators/stratus_hls/fft2_stratus/sw/linux/app/fft2.c
@@ -11,55 +11,27 @@ static unsigned out_offset;
 static unsigned size;
 
 const float ERR_TH = 0.05;
-#include "in_data.h"
-#include "out_data.h"
 
 /* User-defined code */
 static int validate_buffer(token_t *out, float *gold)
 {
 	int j;
-	unsigned errors1 = 0;
-	unsigned errors2 = 0;
-	unsigned errors3 = 0;
+	unsigned errors = 0;
 	const unsigned num_samples = 1<<logn_samples;
 
 	for (j = 0; j < 2 * num_ffts * num_samples; j++) {
 		native_t val = fx2float(out[j], FX_IL);
-		printf("FFT_OUTPUT %u : %f vs %f\n", j, FFT_OUTPUTS[j], val);
-		usleep(50000);
-	}
-
-	for (j = 0; j < 2 * num_ffts * num_samples; j++) {
-		native_t val = fx2float(out[j], FX_IL);
-		/*printf("GOLD[%u] = %f : %f = OUTPUT[%u]\n", j, gold[j], val, j);*/
 
 		if ((fabs(gold[j] - val) / fabs(gold[j])) > ERR_TH) {
-			if (errors1 < 2) { 
+			if (errors < 2) {
 				printf(" GOLD[%u] = %f vs %f = out[%u]\n", j, gold[j], val, j);
 			}
-			errors1++;
+			errors++;
 		}
 	}
-	for (j = 0; j < 2 * num_ffts * num_samples; j++) {
-		native_t val = fx2float(out[j], FX_IL);
-		if ((fabs(FFT_OUTPUTS[j] - val) / fabs(FFT_OUTPUTS[j])) > ERR_TH) {
-			if (errors2 < 2) { 
-				printf(" FFT_OUTPUTS[%u] = %f vs %f = out[%u]\n", j, FFT_OUTPUTS[j], val, j);
-			}
-			errors2++;
-		}
-	}
-	for (j = 0; j < 2 * num_ffts * num_samples; j++) {
-		if ((fabs(FFT_OUTPUTS[j] - gold[j]) / fabs(FFT_OUTPUTS[j])) > ERR_TH) {
-			if (errors3 < 2) { 
-				printf(" FFT_OUTPUTS[%u] = %f vs %f = gold[%u]\n", j, FFT_OUTPUTS[j], gold[j], j);
-			}
-			errors3++;
-		}
-	}
-	printf("  + Relative error > %.02f for %d gold %d output %d GvO values out of %d\n", ERR_TH, errors1, errors2, errors3, 2 * num_ffts * num_samples);
+	printf("  + Relative error > %.02f for %d values out of %d\n", ERR_TH, errors, 2 * num_ffts * num_samples);
 
-	return errors1;
+	return errors;
 }
 
 
@@ -67,17 +39,15 @@ static int validate_buffer(token_t *out, float *gold)
 static void init_buffer(token_t *in, float *gold)
 {
 	int j;
-	//const float LO = -2.0;
-	//const float HI = 2.0;
+	const float LO = -2.0;
+	const float HI = 2.0;
 	const unsigned num_samples = (1 << logn_samples);
 
 	srand((unsigned int) time(NULL));
 
 	for (j = 0; j < 2 * num_ffts * num_samples; j++) {
-		//float scaling_factor = (float) rand () / (float) RAND_MAX;
-		//gold[j] = LO + scaling_factor * (HI - LO);
-		gold[j] = FFT_INPUTS[j];
-		printf("INPUT %u : %f\n", j, gold[j]);
+		float scaling_factor = (float) rand () / (float) RAND_MAX;
+		gold[j] = LO + scaling_factor * (HI - LO);
 	}
 
 	// convert input to fixed point
@@ -87,22 +57,6 @@ static void init_buffer(token_t *in, float *gold)
 
 	// Compute golden output
 	fft2_comp(gold, num_ffts, (1<<logn_samples), logn_samples, do_inverse, do_shift);
-	for (j = 0; j < 2 * num_ffts * num_samples; j++) {
-		printf("GOLD %u : %f\n", j, gold[j]);
-		usleep(50000);
-	}
-	{
-		float    tf_vals[4] = { 1.0, 0.5, 0.25, 1/sqrt(52.0)};
-		token_t  tx_vals[4];
-		for (int ti = 0; ti < 4; ti++) {
-			tx_vals[ti] = float2fx(tf_vals[ti], FX_IL);
-#if (FFT2_FX_WIDTH == 64)
-			printf(" FP2FX: %f -> 0x%16llx\n", tf_vals[ti], *(uint64_t*)(&tx_vals[ti]));
-#elif (FFT2_FX_WIDTH == 32)
-			printf(" FP2FX: %f -> 0x%8x\n", tf_vals[ti], *(uint32_t*)(&tx_vals[ti]));
-#endif
-		}
-	}
 }
 
 
