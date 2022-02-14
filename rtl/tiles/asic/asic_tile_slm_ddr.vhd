0ab9c57eea11b8e22dfa3a439bb991538b4a866f
Joseph Zuckerman <jzuck@cs.columbia.edu>
2022-02-14 12:22:20 -0500

    :bug: fix updated asic_tile_slm_ddr for xcelium

--- rtl/tiles/asic/asic_tile_slm_ddr.vhd
+++ rtl/tiles/asic/asic_tile_slm_ddr.vhd
@@ -205,8 +205,8 @@ architecture rtl of asic_tile_slm_ddr is
   signal ddr_ahbso : ahb_slv_out_type;
 
   signal this_slmddr_id : integer range 0 to SLMDDR_ID_RANGE_MSB;
-  signal this_slmddr_haddr  : integer range 0 to 4096;
-  signal this_slmddr_hmask  : integer range 0 to 4096;
+  signal this_slmddr_haddr  : integer range 0 to 4095;
+  signal this_slmddr_hmask  : integer range 0 to 4095;
 
   constant ext_clk_sel_default : std_ulogic := '0';
 
