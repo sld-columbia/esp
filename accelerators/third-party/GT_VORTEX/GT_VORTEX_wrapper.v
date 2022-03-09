690d5288bc48e611169b17c926e4630e7dbea4df
Srivatsan Raveendran <sr3859@columbia.edu>
2022-03-09 16:05:10 +0000

    updated wrapper

--- accelerators/third-party/GT_VORTEX/GT_VORTEX_wrapper.v
+++ accelerators/third-party/GT_VORTEX/GT_VORTEX_wrapper.v
@@ -106,54 +106,57 @@ module GT_VORTEX_wrapper #(
    ////////////////////////////////////////////////////////////////////////////////
 
 
-   Vortex_axi #(
-	   .AXI_DATA_WIDTH(`VX_MEM_DATA_WIDTH),
-   	   .AXI_ADDR_WIDTH(32),
-           .AXI_TID_WIDTH(`VX_MEM_TAG_WIDTH),
-           .AXI_STROBE_WIDTH(`VX_MEM_DATA_WIDTH / 8)
-   )  vortex_0(
-        .clk            (clk), 
-	.reset		(reset),
-        .m_axi_awid     (m_axi_awid),
-        .m_axi_awaddr   (m_axi_awaddr),
-        .m_axi_awlen    (m_axi_awlen),
-        .m_axi_awsize   (m_axi_awsize),
-        .m_axi_awburst  (m_axi_awburst),
-        .m_axi_awlock   (m_axi_awlock),
-        .m_axi_awcache  (m_axi_awcache),
-        .m_axi_awprot   (m_axi_awprot),
-        .m_axi_awqos    (m_axi_awqos),
-        .m_axi_awvalid  (m_axi_awvalid),
-        .m_axi_awready  (m_axi_awready),
+  Vortex_axi  #(
+    .AXI_DATA_WIDTH    ( `VX_MEM_DATA_WIDTH     ),
+    .AXI_ADDR_WIDTH    (  32                    ),
+    .AXI_TID_WIDTH     ( `VX_MEM_TAG_WIDTH      ),
+    .AXI_STROBE_WIDTH  ( `VX_MEM_DATA_WIDTH / 8 )
+) Vortex_axi_0 (
+    .clk            ( clk   ),
+    .reset          ( reset ),
 
-        .m_axi_wdata    (m_axi_wdata),
-        .m_axi_wstrb    (m_axi_wstrb),
-        .m_axi_wlast    (m_axi_wlast),
-        .m_axi_wvalid   (m_axi_wvalid),
-        .m_axi_wready   (m_axi_wready),
+    .m_axi_awid     ( m_axi_awid    ),
+    .m_axi_awaddr   ( m_axi_awaddr  ),
+    .m_axi_awlen    ( m_axi_awlen   ),
+    .m_axi_awsize   ( m_axi_awsize  ),
+    .m_axi_awburst  ( m_axi_awburst ),
+    .m_axi_awlock   ( m_axi_awlock  ),
+    .m_axi_awcache  ( m_axi_awcache ),
+    .m_axi_awprot   ( m_axi_awprot  ),
+    .m_axi_awqos    ( m_axi_awqos   ),
+    .m_axi_awvalid  ( m_axi_awvalid ),
+    .m_axi_awready  ( m_axi_awready ),
 
-        .m_axi_bid      (m_axi_bid),
-        .m_axi_bresp    (m_axi_bresp),
-        .m_axi_bvalid   (m_axi_bvalid),
-        .m_axi_bready   (m_axi_bready),
+    .m_axi_wdata    ( m_axi_wdata   ),
+    .m_axi_wstrb    ( m_axi_wstrb   ),
+    .m_axi_wlast    ( m_axi_wlast   ),
+    .m_axi_wvalid   ( m_axi_wvalid  ),
+    .m_axi_wready   ( m_axi_wready  ),
 
-        .m_axi_arid     (m_axi_arid),
-        .m_axi_araddr   (m_axi_araddr),
-        .m_axi_arlen    (m_axi_arlen),
-	.m_axi_arsize   (m_axi_arsize),
-        .m_axi_arburst  (m_axi_arburst),
-        .m_axi_arlock   (m_axi_arlock),
-        .m_axi_arcache  (m_axi_arcache),
-        .m_axi_arprot   (m_axi_arprot),
-        .m_axi_arqos    (m_axi_arqos),
-        .m_axi_arvalid  (m_axi_arvalid),
-        .m_axi_arready  (m_axi_arready),
+    .m_axi_bid      ( m_axi_bid     ),
+    .m_axi_bresp    ( m_axi_bresp   ),
+    .m_axi_bvalid   ( m_axi_bvalid  ),
+    .m_axi_bready   ( m_axi_bready  ),
 
-        .m_axi_rid      (m_axi_rid),
-        .m_axi_rdata    (m_axi_rdata),
-        .m_axi_rresp    (m_axi_rresp),
-        .m_axi_rlast    (m_axi_rlast),
-        .m_axi_rvalid   (m_axi_rvalid),
-        .m_axi_rready   (m_axi_rready)
-    );   
+    .m_axi_arid     ( m_axi_arid    ),
+    .m_axi_araddr   ( m_axi_araddr  ),
+    .m_axi_arlen    ( m_axi_arlen   ),
+    .m_axi_arsize   ( m_axi_arsize  ),
+    .m_axi_arburst  ( m_axi_arburst ),
+    .m_axi_arlock   ( m_axi_arlock  ),
+    .m_axi_arcache  ( m_axi_arcache ),
+    .m_axi_arprot   ( m_axi_arprot  ),
+    .m_axi_arqos    ( m_axi_arqos   ),
+    .m_axi_arvalid  ( m_axi_arvalid ),
+    .m_axi_arready  ( m_axi_arready ),
+
+    .m_axi_rid      ( m_axi_rid     ),
+    .m_axi_rdata    ( m_axi_rdata   ),
+    .m_axi_rresp    ( m_axi_rresp   ),
+    .m_axi_rlast    ( m_axi_rlast   ),
+    .m_axi_rvalid   ( m_axi_rvalid  ),
+    .m_axi_rready   ( m_axi_rready  ),
+
+    .busy           ( busy )
+  );  
 endmodule
