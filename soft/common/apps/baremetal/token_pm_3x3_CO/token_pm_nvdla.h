323b40f61c1a6452a7d31472948210413b13362a
Martin Cochet <martin.cochet@ibm.com>
2022-04-21 11:51:23 -0400

    fixed NVLDA setup on 3x3CO

--- soft/common/apps/baremetal/token_pm_3x3_CO/token_pm_nvdla.h
+++ soft/common/apps/baremetal/token_pm_3x3_CO/token_pm_nvdla.h
@@ -358,8 +358,8 @@ void run_nvdla(struct esp_device *espdev, struct esp_device *dev, nvdla_token_t 
         setup_nvdla(mem, i_base, o_base, b_base, w_base);
         //time1 = nvdla_get_counter();
         plic_dev.addr = PLIC_ADDR;
-        while(ioread32(&plic_dev, PLIC_IP_OFFSET) != 0x40);
-	printf("Wait\n");
+		while((ioread32(&plic_dev, PLIC_IP_OFFSET) & 0x40) == 0);	
+		printf("Wait\n");
         //time2 = nvdla_get_counter();
 
         //printf("Time load: %llu\n", time1 - time0);
