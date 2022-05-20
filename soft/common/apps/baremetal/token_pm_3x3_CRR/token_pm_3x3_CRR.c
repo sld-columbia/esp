6b396c279f3126d4999f8e0619b0c9956941f5e2
Martin Cochet <martin.cochet@ibm.com>
2022-05-19 21:21:07 -0400

    CRR updates

--- soft/common/apps/baremetal/token_pm_3x3_CRR/token_pm_3x3_CRR.c
+++ soft/common/apps/baremetal/token_pm_3x3_CRR/token_pm_3x3_CRR.c
@@ -1,19 +1,20 @@
 /* Copyright (c) 2011-2021 Columbia University, System Level Design Group */
 /* SPDX-License-Identifier: Apache-2.0 */
 
-#include <stdio.h>
+//#include <stdio.h>
 #ifndef __riscv
 #include <stdlib.h>
 #endif
 
 #include <esp_accelerator.h>
 #include <esp_probe.h>
+//#include "token_pm_3x3_Ccommon.h"
 #include "token_pm_3x3_CRR.h"
-#include "token_pm_3x3_Ccommon.h"
-#include "token_pm_3x3_SW_data.h"
+//#include "token_pm_3x3_SW_data.h"
 #include "token_pm_fft.h"
 #include "token_pm_vitdodec.h"
-#include "token_pm_nvdla.h"
+
+//Global
 
 int main(int argc, char * argv[])
 {
@@ -25,8 +26,7 @@ int main(int argc, char * argv[])
 
 
     unsigned acc_tile_pm_csr_addr[N_ACC];
-    struct esp_device espdevs[N_ACC];
-    struct esp_device *espdev;
+
 
     // setup CSR base addresses
     for (i = 0; i < N_ACC; i++) {
@@ -51,19 +51,123 @@ int main(int argc, char * argv[])
 #endif
 
 #ifdef TEST_1
+//#define DEBUG 1
+
 //Test with dummy activity. In progress
-	p_available=P_TOTAL;
-	Pmax[N_ACC] = {max_tokens_NVDLA,max_tokens_FFT,max_tokens_VIT,max_tokens_FFT,max_tokens_VIT,max_tokens_FFT};
-	Fmax[N_ACC] = {lut_data_const_NVDLA[max_tokens_NVDLA],lut_data_const_FFT[max_tokens_FFT],lut_data_const_VIT[max_tokens_VIT],lut_data_const_FFT[max_tokens_FFT],lut_data_const_VIT[max_tokens_VIT],lut_data_const_FFT[max_tokens_FFT]};
-	Fmin[N_ACC] = {lut_data_const_NVDLA[0],lut_data_const_FFT[0],lut_data_const_VIT[0],lut_data_const_FFT[0],lut_data_const_VIT[0],lut_data_const_FFT[0]};
+    printf("Test 1: Starting 4/17\n");
+
+
+   //nvdla_token_t *mem_n1;
+   //nvdla_token_t *gold_nvdla;
+   
+   fft_token_t *mem_f1, *mem_f2, *mem_f3;
+   fft_token_t *gold_fft;
+   vit_token_t *mem_v1, *mem_v2;
+   vit_token_t *gold_vit;
+
+   unsigned errors = 0;
+   unsigned coherence = ACC_COH_RECALL;
+   const int ERROR_COUNT_TH = 0.001;
+   uint64_t cycles_start = 0, cycles_end_n1 = 0, cycles_end_f1 = 0,cycles_end_v1 = 0, cycles_end_f2 = 0, cycles_end_v2 = 0, cycles_end_f3 = 0;
+   int ndev;
+    
+   unsigned **ptable_f1 = NULL;
+   unsigned **ptable_f2 = NULL;
+   unsigned **ptable_f3 = NULL;
+   unsigned **ptable_v1 = NULL;
+   unsigned **ptable_v2 = NULL;
+
+   printf("Test 1: Starting debug CRR\n");
 	
-   struct esp_device *dev_n1 = aligned_malloc(sizeof(struct esp_device));
-   struct esp_device *dev_f1 = aligned_malloc(sizeof(struct esp_device));
-   struct esp_device *dev_f2 = aligned_malloc(sizeof(struct esp_device));
-   struct esp_device *dev_f3 = aligned_malloc(sizeof(struct esp_device));
-   struct esp_device *dev_v1 = aligned_malloc(sizeof(struct esp_device));
-   struct esp_device *dev_v2 = aligned_malloc(sizeof(struct esp_device));	
-	struct esp_device **dev_list[N_ACC]={dev_n0,dev_f1,dev_v1,dev_f2,dev_v2,dev_f3};
+	for (i=0;i<N_ACC;i++){
+		dev_list_acc[i]=aligned_malloc(sizeof(struct esp_device));
+	}
+	
+
+   ////FFT setup/////
+   #ifdef DEBUG
+   	printf("Setting up FFT accelerators\n");
+   #endif
+   mem_size=fft_init_params();
+   dev_list_acc[1]->addr = ACC_ADDR_FFT1;
+   setup_fft(dev_list_acc[1], gold_fft, mem_f1, ptable_f1);
+   #ifdef DEBUG
+   	printf("FFT1 setup complete, address=0x%x\n",dev_list_acc[1]->addr);
+   #endif
+   dev_list_acc[3]->addr = ACC_ADDR_FFT2;
+   setup_fft(dev_list_acc[3], gold_fft, mem_f2, ptable_f2);
+   #ifdef DEBUG
+   	printf("FFT2 setup complete, address=0x%x\n",dev_list_acc[3]->addr);
+   #endif
+   dev_list_acc[5]->addr = ACC_ADDR_FFT3;
+   setup_fft(dev_list_acc[5], gold_fft, mem_f3, ptable_f3);
+   #ifdef DEBUG
+   	printf("FFT3 setup complete, address=0x%x\n",dev_list_acc[5]->addr);
+   #endif
+   //printf("FFT setup complete\n");
+
+    //FFT is all set
+	
+   //////Viterbi setup//////
+	
+    #ifdef DEBUG
+    	printf("Starting viterbi init\n");
+    #endif
+    mem_size=vit_init_params();
+    dev_list_acc[2]->addr = ACC_ADDR_VITERBI1;
+    setup_viterbi(dev_list_acc[2], gold_vit, mem_v1, ptable_v1);
+    #ifdef DEBUG
+    	printf("Viterbi1 setup complete, address=0x%x\n",dev_list_acc[2]->addr);
+    #endif
+    dev_list_acc[4]->addr = ACC_ADDR_VITERBI2;
+    setup_viterbi(dev_list_acc[4], gold_vit, mem_v2, ptable_v2);
+    #ifdef DEBUG
+    	printf("Viterbi2 setup complete, address=0x%x\n",dev_list_acc[4]->addr);
+    #endif
+    //printf("Viterbi setup complete\n");
+    
+    //////NVDLA setup//////
+    #ifdef DEBUG
+    	printf("NVDLA init and start\n");
+    #endif
+    dev_list_acc[0]->addr = ACC_ADDR_NVDLA;
+
+    // Flush (customize coherence model here)
+    if (coherence != ACC_COH_RECALL)
+        esp_flush(coherence, 1);
+
+	//Start accelerators 1 to 5
+	for (i=1;i<N_ACC;i++){
+		start_tile(i);
+	}
+	
+	//Start NVDLA
+	write_config1(&espdevs[0], 1, 0, 0, 0);
+
+	//run_nvdla( &espdevs[0], dev_list_acc[0], gold_nvdla, mem_n1, 0);
+	start_tile(0);
+	
+	//Wait for NVDLA done
+	write_config1(&espdevs[0], 0, 0, 0, 0);
+	set_freq(&espdevs[0],Fmin[0]);
+	p_available=p_available+Pmin[0];
+	removeFromList(&head_idle,0);
+	#ifdef DEBUG
+		printf("Finished NVDLA new Pav %u\n",p_available);
+    #endif
+
+    #ifdef DEBUG
+		printf("Finished N1\n");
+    #endif
+
+	
+	while((head_run != NULL) ||(head_idle != NULL) ||(head_wait != NULL) ) {
+		CRR_step_checkend();
+		CRR_step_rotate();
+		printf("Run step\n");
+	
+	}
+		
 	
 #endif
 
--- soft/common/apps/baremetal/token_pm_3x3_CRR/token_pm_3x3_CRR.h
+++ soft/common/apps/baremetal/token_pm_3x3_CRR/token_pm_3x3_CRR.h
@@ -1,47 +1,233 @@
-unsigned p_available;
-const unsigned Pmax[N_ACC];
-const unsigned Fmax[N_ACC];
-const unsigned Fmin[N_ACC];
-struct esp_device **dev_list = aligned_malloc(N_ACC*sizeof((struct *)esp_device));
+#include "token_pm_3x3_Ccommon.h"
+#include "token_pm_3x3_SW_data.h"
+#include "token_pm_nvdla.h"
 
+unsigned p_available = P_TOTAL;
 
-void start_tile(struct esp_device espdevs[], unsigned i)
+const unsigned Pmax[N_ACC] = {max_power_NVDLA,max_power_FFT,max_power_VIT,max_power_FFT,max_power_VIT,max_power_FFT};
+const unsigned Fmax[N_ACC]= {lut_data_const_NVDLA[max_tokens_NVDLA],lut_data_const_FFT[max_tokens_FFT],lut_data_const_VIT[max_tokens_VIT],lut_data_const_FFT[max_tokens_FFT],lut_data_const_VIT[max_tokens_VIT],lut_data_const_FFT[max_tokens_FFT]};
+const unsigned Fmin[N_ACC] = {lut_data_const_NVDLA[0],lut_data_const_FFT[0],lut_data_const_VIT[0],lut_data_const_FFT[0],lut_data_const_VIT[0],lut_data_const_FFT[0]};
+const unsigned Pmin[N_ACC] = {min_power_NVDLA,min_power_FFT,min_power_VIT,min_power_FFT,min_power_VIT,min_power_FFT};
+
+struct esp_device espdevs[N_ACC];
+struct esp_device *dev_list_acc[N_ACC]; 
+unsigned done[N_ACC];
+unsigned done_before[N_ACC];
+	
+
+struct node
 {
-	if(Pmax[i]<p_available) {
-		set_freq(&espdevs[i],Fmax[i]);
-		p_available=p_available-Pmax[i];
-	}
-	else
-	{
-		set_freq(&espdevs[i],Fmin[i]);
-	}
-	write_config1(&espdevs[i],1,0,0,0);
-	//Add to running list
+  int data;
+  struct node *next;
+};
+
+struct node *head_run;
+struct node *head_idle;
+struct node *head_wait;
+
+
+
+nvdla_token_t *mem_n1;
+nvdla_token_t *gold_nvdla;
+   
+   
+void addLast(struct node **head, int val)
+{
+	//From https://www.log2base2.com/data-structures/linked-list/inserting-a-node-at-the-end-of-a-linked-list.html
+    //create a new node
+    struct node *newNode = aligned_malloc(sizeof(struct node));
+    newNode->data = val;
+    newNode->next = NULL;
+
+    //if head is NULL, it is an empty list
+    if(*head == NULL)
+         *head = newNode;
+    //Otherwise, find the last node and add the newNode
+    else
+    {
+        struct node *lastNode = *head;
+        //last node's next address will be NULL.
+        while(lastNode->next != NULL)
+        {
+            lastNode = lastNode->next;
+        }
+
+        //add the newNode at the end of the linked list
+        lastNode->next = newNode;   
+    }
 }
 
-void CRR_step_checkend(struct esp_device espdevs[], unsigned i)
+int popFirst(struct node **head)
 {
-	unsigned done_i=0;
-	unsigned done_i_before=0;
+ int result;
+ struct node *headNode = *head;
 
- 	for (i = 0; i < N_ACC; i++) {   
-   			//if(i==1)
+ if(*head == NULL)
+ 	return -1;
+ else
+ 	{
+	result=(headNode->data);
+    *head = (headNode->next);
+ 	return (result);
+	}
+}
 
-			//else if(i==2)
-			
-        	done_i = ioread32(i, STATUS_REG);
-			done_i &= STATUS_MASK_DONE;
+
+int removeFromList(struct node **head,int index)
+{
+    struct node *current_node = *head;
+   	if ( current_node != NULL) {
+       	if((current_node->data)==index){
+    		//printf("Found!!\n");
+        	*head=(current_node->next);
+			return 1;
+		}
+		else {
+			removeFromList(&(current_node->next),index);
+			}
+    }
+	else {return 0;}
+}
+
+void start_tile( unsigned i)
+{
+	if(Pmax[i]<=p_available) {
+		set_freq(&espdevs[i],Fmax[i]);
+		p_available=p_available-Pmax[i];
+		#ifdef DEBUG
+			printf("Starting tile %u at Pmax new Pav %u\n",i,p_available);
+		#endif
+
+		addLast(&head_run,i);
+		if(i>0){
+			iowrite32(dev_list_acc[i], CMD_REG, CMD_MASK_START);
+			write_config1(&espdevs[i],1,0,0,0);}
+		else{
+			run_nvdla( &espdevs[0], dev_list_acc[0], gold_nvdla, mem_n1, 0);
+		}
+	}
+	else if(Pmin[i]<=p_available) {
+		set_freq(&espdevs[i],Fmin[i]);
+		p_available=p_available-Pmin[i];
+		#ifdef DEBUG
+			printf("Starting tile %u at Pmin new Pav %u\n",i,p_available);
+		#endif
 		
-		if(done_i && !done_i_before) {
+		addLast(&head_idle,i);
+		if(i>0){
+			iowrite32(dev_list_acc[i], CMD_REG, CMD_MASK_START);
+			write_config1(&espdevs[i],1,0,0,0);}
+		else{
+			run_nvdla( &espdevs[0], dev_list_acc[0], gold_nvdla, mem_n1, 0);
+		}	
+		}
+	else {
+		set_freq(&espdevs[i],Fmin[i]);
+		#ifdef DEBUG
+			printf("Waiting tile %u at new Pav %u\n",i,p_available);
+		#endif
+
+		//set_freq(&espdevs[i],Fmin[i]);
+		addLast(&head_wait,i);
+	}
+	
+}
+
+void CRR_step_checkend()
+{
+	int i;
+	int i_run;
+	int i_idl;
+
+ 	for (i = 1; i < N_ACC; i++) {  //Starting at 1 because of NVDLA
+        	done[i] = ioread32(dev_list_acc[i], STATUS_REG);
+			done[i] &= STATUS_MASK_DONE;
+		
+		if(done[i] && !done_before[i]) {
 			write_config1(&espdevs[i], 0, 0, 0, 0);
-			done_i_before=done_i;
-			set_freq(&espdevs[i],Fmin[1]);
-			p_available=p_available+Pmax[i];
-    			#ifdef DEBUG
-				printf("Finished F1\n");
-    			#endif
+			done_before[i]=done[i];
+			set_freq(&espdevs[i],Fmin[i]);
+			i_run=removeFromList(&head_run,i);
+			i_idl=removeFromList(&head_idle,i);
+			if(i_run) {
+			p_available=p_available+Pmax[i];}
+			else if(i_idl) {
+			p_available=p_available+Pmin[i];}	
+			#ifdef DEBUG
+				printf("Finished tile %u was in %u%u new Pav %u\n",i,i_run,i_idl, p_available);
+			#endif
+
 		}
 	}
 }
 
+void CRR_step_rotate()
+{
+	int index_stop;
+	if(head_run != NULL && ((head_idle != NULL) || (head_wait != NULL)  )){
+		index_stop=popFirst(&head_run);
+		addLast(&head_idle,index_stop);
+		set_freq(&espdevs[index_stop],Fmin[index_stop]);
+		p_available=p_available+Pmax[index_stop]-Pmin[index_stop];
+		#ifdef DEBUG
+			printf("Idling tile %u new Pav %u\n",index_stop,p_available);
+		#endif
 
+	}
+    struct node *idleNode = head_idle;
+    struct node *waitNode = head_wait;		
+           while(waitNode!= NULL)
+        {
+            if(Pmax[waitNode->data]<=p_available){
+				set_freq(dev_list_acc[waitNode->data],Fmax[waitNode->data]);
+				if((waitNode->data)>0){
+					iowrite32(dev_list_acc[waitNode->data], CMD_REG, CMD_MASK_START);
+					write_config1(&espdevs[waitNode->data],1,0,0,0);}
+				else{
+					run_nvdla( &espdevs[0], dev_list_acc[0], gold_nvdla, mem_n1, 0);
+				}
+				write_config1(&espdevs[waitNode->data],1,0,0,0);
+				addLast(&head_run,waitNode->data);
+				removeFromList(&head_wait,waitNode->data);
+				p_available=p_available-Pmax[waitNode->data];
+				#ifdef DEBUG
+					printf("wait->run tile %u new Pav %u\n",waitNode->data,p_available);
+				#endif
+
+			}
+			else if(Pmin[waitNode->data]<=p_available){
+				if(waitNode->data>0){
+					iowrite32(dev_list_acc[waitNode->data], CMD_REG, CMD_MASK_START);
+					write_config1(&espdevs[waitNode->data],1,0,0,0);}
+				else{
+					run_nvdla( &espdevs[0], dev_list_acc[0], gold_nvdla, mem_n1, 0);
+				}
+				removeFromList(&head_wait,waitNode->data);
+				write_config1(&espdevs[waitNode->data],1,0,0,0);
+				addLast(&head_idle,waitNode->data);
+				p_available=p_available-Pmin[waitNode->data];
+				#ifdef DEBUG
+					printf("wait->idle tile %u new Pav %u\n",waitNode->data,p_available);
+				#endif
+			}
+		waitNode = waitNode->next;
+        }
+		
+		while(idleNode!= NULL)
+        {
+            if(Pmax[idleNode->data]<=p_available){
+				set_freq(dev_list_acc[idleNode->data],Fmax[idleNode->data]);
+				addLast(&head_run,idleNode->data);
+				removeFromList(&head_idle,idleNode->data);
+				p_available=p_available-Pmax[idleNode->data];
+				#ifdef DEBUG
+					printf("Idle->run tile %u new Pav %u\n",idleNode->data,p_available);
+				#endif
+			}
+		idleNode = idleNode->next;
+        }
+		
+		
+
+
+}
