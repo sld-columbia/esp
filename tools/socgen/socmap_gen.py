ff74f2baf2ad05613aa19f5c2891ea441e7ce087
biruk <forbiruk@gmail.com>
2022-08-26 16:30:44 -0400

    fix for axislv2noc proxy

--- tools/socgen/socmap_gen.py
+++ tools/socgen/socmap_gen.py
@@ -708,8 +708,13 @@ def print_constants(fp, soc, esp_config):
     fp.write("  ------ PRC interrupt line\n")
     if soc.prc.get() == 1:
         fp.write("  constant CFG_PRC   : integer := 1;\n")
+        if soc.CPU_ARCH.get() == "leon3":
+            fp.write("  constant IS_PRC_MST : integer range 0 to 1 := 0;\n")
+        else:
+            fp.write("  constant IS_PRC_MST : integer range 0 to 1 := 1;\n")
     else:
         fp.write("  constant CFG_PRC   : integer := 0;\n")
+        fp.write("  constant IS_PRC_MST : integer range 0 to 1 := 0;\n")
     fp.write("  constant CFG_PRC_IRQ : integer := 5;\n")
 
     #
