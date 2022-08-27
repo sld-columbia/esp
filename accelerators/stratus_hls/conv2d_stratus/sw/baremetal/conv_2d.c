38aab4127ef1a11ba100e1692bb15ff09fa63bc0
biruk <forbiruk@gmail.com>
2022-08-27 11:09:08 -0400

    reverted changes to accelerators

--- accelerators/stratus_hls/conv2d_stratus/sw/baremetal/conv_2d.c
+++ accelerators/stratus_hls/conv2d_stratus/sw/baremetal/conv_2d.c
@@ -24,7 +24,7 @@
 // #define BITWIDTH 64
 #else
 #define BITWIDTH 32
-// #define BITWIDTH 64
+//#define BITWIDTH 64
 #endif
 
 /* End of user defined */
@@ -67,7 +67,11 @@ typedef double token_t;
 #endif
 #endif
 
+#if (BITWIDTH == 32)
 typedef float native_t;
+#elif (BITWIDTH == 64)
+typedef double native_t;
+#endif
 
 static unsigned DMA_WORD_PER_BEAT(unsigned _st)
 {
@@ -75,27 +79,42 @@ static unsigned DMA_WORD_PER_BEAT(unsigned _st)
 }
 
 #define MAX_PRINTED_ERRORS 10
-
-#define SLD_GEMM 0x051
-#define DEV_NAME "sld,gemm_stratus"
+#define REL_ERROR_THRESHOLD 0.01
+#define SLD_CONV2D 0x052
+#define DEV_NAME "sld,conv2d_stratus"
 
 /* <<--params-->> */
+const int32_t n_channels = 2;
+const int32_t feature_map_height = 6;
+const int32_t feature_map_width = 6;
+const int32_t n_filters = 2;
+const int32_t filter_height = 3;
+const int32_t filter_width = 3;
+const int32_t pad_h = 1;
+const int32_t pad_w = 1;
+const int32_t is_padded = 1;
+const int32_t stride_h = 1;
+const int32_t stride_w = 1;
+const int32_t dilation_h = 1;
+const int32_t dilation_w = 1;
 const int32_t do_relu = 0;
-const int32_t transpose = 1;
-const int32_t ninputs = 2;
-const int32_t d3 = 8;
-const int32_t d2 = 8;
-const int32_t d1 = 8;
-int32_t st_offset;
-const int32_t ld_offset1 = 0;
-int32_t ld_offset2;
+const int32_t pool_type = 0;
+const int32_t batch_size = 1;
 
 static unsigned in_words_adj;
+static unsigned weights_words_adj;
+static unsigned bias_words_adj;
 static unsigned out_words_adj;
 static unsigned in_len;
+static unsigned weights_len;
+static unsigned bias_len;
 static unsigned out_len;
 static unsigned in_size;
+static unsigned weights_size;
+static unsigned bias_size;
 static unsigned out_size;
+static unsigned weights_offset;
+static unsigned bias_offset;
 static unsigned out_offset;
 static unsigned mem_size;
 
@@ -108,15 +127,16 @@ static unsigned mem_size;
 
 /* User defined registers */
 /* <<--regs-->> */
-#define GEMM_TRANSPOSE_REG 0x60
-#define GEMM_DO_RELU_REG 0x5c
-#define GEMM_ST_OFFSET_REG 0x58
-#define GEMM_LD_OFFSET2_REG 0x54
-#define GEMM_LD_OFFSET1_REG 0x50
-#define GEMM_D3_REG 0x4c
-#define GEMM_D2_REG 0x48
-#define GEMM_D1_REG 0x44
-#define GEMM_NINPUTS_REG 0x40
+#define CONV2D_N_CHANNELS_REG 0x40
+#define CONV2D_FEATURE_MAP_HEIGHT_REG 0x44
+#define CONV2D_FEATURE_MAP_WIDTH_REG 0x48
+#define CONV2D_N_FILTERS_REG 0x4c
+#define CONV2D_FILTER_DIM_REG 0x50
+#define CONV2D_IS_PADDED_REG 0x54
+#define CONV2D_STRIDE_REG 0x58
+#define CONV2D_DO_RELU_REG 0x5c
+#define CONV2D_POOL_TYPE_REG 0x60
+#define CONV2D_BATCH_SIZE_REG 0x64
 
 static int validate_buf(token_t *out, native_t *gold)
 {
@@ -130,12 +150,14 @@ static int validate_buf(token_t *out, native_t *gold)
 #else
             val = out[j];
 #endif
-
-            if (gold[j] != val) {
+            if (!gold[j] && val ||
+        (((gold[j] - val) / gold[j]) > REL_ERROR_THRESHOLD ||
+        ((gold[j] - val) / gold[j]) < -REL_ERROR_THRESHOLD))
+        {
                 errors++;
                 if (errors <= MAX_PRINTED_ERRORS) {
-            printf("%d : %d : %d\n", j, (int) val, (int) gold[j]);
-        }
+                    printf("%d : %d\n", (int) val, (int) gold[j]);
+                }
             }
     }
 
@@ -145,18 +167,15 @@ static int validate_buf(token_t *out, native_t *gold)
 
 static void init_buf (token_t *in, native_t * gold)
 {
-    int i;
-
 #include "input.h"
-
-#ifdef __FIXED
-    for (i = 0; i < ninputs * (d1*d2 + d2*d3); i++) {
-        in[i] = float2fx(in[i], FX_IL);
-    }
-#endif
-
 #include "gold.h"
+
+    int i, size;
+    size = (n_channels * feature_map_height * feature_map_width) +
+    (n_channels * n_filters * filter_height * filter_width) +
+    n_filters;
 }
+
 
 int main(int argc, char * argv[])
 {
@@ -172,29 +191,42 @@ int main(int argc, char * argv[])
     unsigned errors = 0;
     unsigned coherence;
 
-    st_offset = ninputs * (d1 * d2 + d2 * d3);
-    ld_offset2 = ninputs * (d1 * d2);
-    
-    if (DMA_WORD_PER_BEAT(sizeof(token_t)) <= 1) {
-        in_words_adj = ninputs * (d1*d2 + d2*d3);
-        out_words_adj = ninputs * d1 * d3;
+    // Input data and golden output (aligned to DMA_WIDTH makes your life easier)
+    if (DMA_WORD_PER_BEAT(sizeof(token_t)) == 0) {
+        in_words_adj = n_channels * feature_map_height * feature_map_width;
+        weights_words_adj = n_filters * n_channels * filter_height * filter_width;
+        bias_words_adj = n_filters;
+        out_words_adj = n_filters * feature_map_height * feature_map_width;
     } else {
-        in_words_adj = round_up(ninputs * (d1*d2 + d2*d3), DMA_WORD_PER_BEAT(sizeof(token_t)));
-        out_words_adj = round_up(ninputs * d1 * d3, DMA_WORD_PER_BEAT(sizeof(token_t)));
+        in_words_adj = round_up(n_channels * feature_map_height * feature_map_width,
+                    DMA_WORD_PER_BEAT(sizeof(token_t)));
+        weights_words_adj = round_up(n_filters * n_channels * filter_height * filter_width,
+                     DMA_WORD_PER_BEAT(sizeof(token_t)));
+        bias_words_adj = round_up(n_filters, DMA_WORD_PER_BEAT(sizeof(token_t)));
+        out_words_adj = round_up(n_filters * feature_map_height * feature_map_width,
+                     DMA_WORD_PER_BEAT(sizeof(token_t)));
     }
-    in_len = in_words_adj;
-    out_len = out_words_adj;
+
+    in_len = in_words_adj * (1);
+    weights_len = weights_words_adj * (1);
+    bias_len = bias_words_adj * (1);
+    out_len = out_words_adj * (1);
     in_size = in_len * sizeof(token_t);
+    weights_size = weights_len * sizeof(token_t);
+    bias_size = bias_len * sizeof(token_t);
     out_size = out_len * sizeof(token_t);
-    out_offset  = in_len;
-    mem_size = (out_offset * sizeof(token_t)) + out_size;
+    weights_offset = in_len;
+    bias_offset = in_len + weights_len;
+    out_offset  = in_len + weights_len + bias_len;
+    mem_size = in_size + weights_size + bias_len + out_size;
+
 
     // Search for the device
     printf("Scanning device tree... \n");
 
-    ndev = probe(&espdevs, VENDOR_SLD, SLD_GEMM, DEV_NAME);
+    ndev = probe(&espdevs, VENDOR_SLD, SLD_CONV2D, DEV_NAME);
     if (ndev == 0) {
-        printf("gemm not found\n");
+        printf("conv2d not found\n");
         return 0;
     }
 
@@ -216,14 +248,13 @@ int main(int argc, char * argv[])
         // Allocate memory
         gold = aligned_malloc(out_size);
         mem = aligned_malloc(mem_size);
+
         printf("  memory buffer base-address = %p\n", mem);
-        printf("  memory buffer base-address for gold = %p\n", gold);
 
         // Allocate and populate page table
         ptable = aligned_malloc(NCHUNK(mem_size) * sizeof(unsigned *));
         for (i = 0; i < NCHUNK(mem_size); i++)
             ptable[i] = (unsigned *) &mem[i * (CHUNK_SIZE / sizeof(token_t))];
-
         printf("  ptable = %p\n", ptable);
         printf("  nchunk = %lu\n", NCHUNK(mem_size));
 
@@ -245,32 +276,31 @@ int main(int argc, char * argv[])
             iowrite32(dev, COHERENCE_REG, coherence);
 
             iowrite32(dev, PT_ADDRESS_REG, (unsigned long) ptable);
-
             iowrite32(dev, PT_NCHUNK_REG, NCHUNK(mem_size));
             iowrite32(dev, PT_SHIFT_REG, CHUNK_SHIFT);
 
             // Use the following if input and output data are not allocated at the default offsets
-            iowrite32(dev, SRC_OFFSET_REG, 0);
-            iowrite32(dev, DST_OFFSET_REG, 0);
+            iowrite32(dev, SRC_OFFSET_REG, 0x0);
+            iowrite32(dev, DST_OFFSET_REG, 0x0);
 
-            // Pass accelerator-specific configuration parametxers
+            // Pass accelerator-specific configuration parameters
             /* <<--regs-config-->> */
-            iowrite32(dev, GEMM_DO_RELU_REG, do_relu);
-            iowrite32(dev, GEMM_TRANSPOSE_REG, transpose);
-            iowrite32(dev, GEMM_NINPUTS_REG, ninputs);
-            iowrite32(dev, GEMM_D3_REG, d3);
-            iowrite32(dev, GEMM_D2_REG, d2);
-            iowrite32(dev, GEMM_D1_REG, d1);
-            iowrite32(dev, GEMM_ST_OFFSET_REG, st_offset);
-            iowrite32(dev, GEMM_LD_OFFSET1_REG, ld_offset1);
-            iowrite32(dev, GEMM_LD_OFFSET2_REG, ld_offset2);
+            iowrite32(dev, CONV2D_N_CHANNELS_REG, n_channels);
+            iowrite32(dev, CONV2D_FEATURE_MAP_HEIGHT_REG, feature_map_height);
+            iowrite32(dev, CONV2D_FEATURE_MAP_WIDTH_REG, feature_map_width);
+            iowrite32(dev, CONV2D_N_FILTERS_REG, n_filters);
+            iowrite32(dev, CONV2D_FILTER_DIM_REG, filter_height);
+            iowrite32(dev, CONV2D_IS_PADDED_REG, is_padded);
+            iowrite32(dev, CONV2D_STRIDE_REG, stride_w);
+            iowrite32(dev, CONV2D_DO_RELU_REG, do_relu);
+            iowrite32(dev, CONV2D_POOL_TYPE_REG, pool_type);
+            iowrite32(dev, CONV2D_BATCH_SIZE_REG, batch_size);
 
             // Flush (customize coherence model here)
             esp_flush(coherence);
 
             // Start accelerators
             printf("  Start...\n");
-
             iowrite32(dev, CMD_REG, CMD_MASK_START);
 
             // Wait for completion
@@ -286,7 +316,6 @@ int main(int argc, char * argv[])
 
             /* Validation */
             errors = validate_buf(&mem[out_offset], gold);
-
             if (errors)
                 printf("  ... FAIL\n");
             else
--- accelerators/stratus_hls/vitdodec_stratus/sw/baremetal/vitdodec.c
+++ accelerators/stratus_hls/vitdodec_stratus/sw/baremetal/vitdodec.c
@@ -11,8 +11,6 @@
 
 #include "do_decoding.h"
 
-// #define LARGE_WORKLOAD
-
 typedef int8_t token_t;
 
 static unsigned DMA_WORD_PER_BEAT(unsigned _st)
@@ -27,11 +25,8 @@ static unsigned DMA_WORD_PER_BEAT(unsigned _st)
 /* <<--params-->> */
 const int32_t cbps = 48;
 const int32_t ntraceback = 5;
-#ifndef LARGE_WORKLOAD
 const int32_t data_bits = 288;
-#else
-const int32_t data_bits = 18585;
-#endif
+
 static unsigned in_words_adj;
 static unsigned out_words_adj;
 static unsigned in_len;
@@ -44,9 +39,9 @@ static unsigned mem_size;
 /* Size of the contiguous chunks for scatter/gather */
 #define CHUNK_SHIFT 20
 #define CHUNK_SIZE BIT(CHUNK_SHIFT)
-#define NCHUNK(_sz) ((_sz % CHUNK_SIZE == 0) ?		\
-			(_sz / CHUNK_SIZE) :		\
-			(_sz / CHUNK_SIZE) + 1)
+#define NCHUNK(_sz) ((_sz % CHUNK_SIZE == 0) ?      \
+            (_sz / CHUNK_SIZE) :        \
+            (_sz / CHUNK_SIZE) + 1)
 
 /* User defined registers */
 /* <<--regs-->> */
@@ -93,16 +88,16 @@ static const unsigned char PARTAB[256] = {
 
 static int validate_buf(token_t *out, token_t *gold)
 {
-	int i;
-	int j;
-	unsigned errors = 0;
+    int i;
+    int j;
+    unsigned errors = 0;
 
-	for (i = 0; i < 1; i++)
-		for (j = 0; j < 18585; j++)
-			if (gold[i * out_words_adj + j] != out[i * out_words_adj + j])
-				errors++;
+    for (i = 0; i < 1; i++)
+        for (j = 0; j < 18585; j++)
+            if (gold[i * out_words_adj + j] != out[i * out_words_adj + j])
+                errors++;
 
-	return errors;
+    return errors;
 }
 
 
@@ -112,20 +107,20 @@ static int validate_buf(token_t *out, token_t *gold)
 static unsigned long int next = 42;
 int irand(void)
 {
-	unsigned int rand_tmp;
-	next = next * 1103515245 + 12345;
-	rand_tmp = (unsigned int) (next / 65536) % 32768;
-	return ((int) rand_tmp);
+    unsigned int rand_tmp;
+    next = next * 1103515245 + 12345;
+    rand_tmp = (unsigned int) (next / 65536) % 32768;
+    return ((int) rand_tmp);
 
 }
 
 static void init_buf (token_t *in, token_t * gold)
 {
-	int i;
-	int j;
-	int imi = 0;
+    int i;
+    int j;
+    int imi = 0;
 
-	unsigned char depunct_ptn[6] = {1, 1, 0, 0, 0, 0}; /* PATTERN_1_2 Extended with zeros */
+    unsigned char depunct_ptn[6] = {1, 1, 0, 0, 0, 0}; /* PATTERN_1_2 Extended with zeros */
 
         int polys[2] = { 0x6d, 0x4f };
         for(int i=0; i < 32; i++) {
@@ -147,70 +142,70 @@ static void init_buf (token_t *in, token_t * gold)
         in[imi++] = 0;
 
         for (int j = imi; j < in_size; j++) {
-	    int bval = irand()  & 0x01;
+        int bval = irand()  & 0x01;
             /* printf("Setting up in[%d] = %d\n", j, bval); */
             in[j] = bval;
         }
 
 #if(0)
-	{
-		printf(" memory in   = ");
-		int limi = 0;
-		for (int li = 0; li < 32; li++) {
-			printf("%u", in[limi++]);
-			if ((li % 8) == 7) { printf(" "); }
-		}
-		printf("\n      brtb1    ");
-		for (int li = 0; li < 32; li++) {
-			printf("%u", in[limi++]);
-			if ((li % 8) == 7) { printf(" "); }
-		}
-		printf("\n      depnc    ");
-		for (int li = 0; li < 8; li++) {
-			printf("%u", in[limi++]);
-		}
-		printf("\n      depdta   ");
-		for (int li = 0; li < 32; li++) {
-			printf("%u", in[limi++]);
-			if ((li % 8) == 7) { printf(" "); }
-		}
-		printf("\n");
+    {
+        printf(" memory in   = ");
+        int limi = 0;
+        for (int li = 0; li < 32; li++) {
+            printf("%u", in[limi++]);
+            if ((li % 8) == 7) { printf(" "); }
+        }
+        printf("\n      brtb1    ");
+        for (int li = 0; li < 32; li++) {
+            printf("%u", in[limi++]);
+            if ((li % 8) == 7) { printf(" "); }
+        }
+        printf("\n      depnc    ");
+        for (int li = 0; li < 8; li++) {
+            printf("%u", in[limi++]);
+        }
+        printf("\n      depdta   ");
+        for (int li = 0; li < 32; li++) {
+            printf("%u", in[limi++]);
+            if ((li % 8) == 7) { printf(" "); }
+        }
+        printf("\n");
         }
 #endif
-	/* Pre-zero the output memeory */
-	for (i = 0; i < 1; i++)
-		for (j = 0; j < 18585; j++)
-			gold[i * out_words_adj + j] = (token_t) 0;
+    /* Pre-zero the output memeory */
+    for (i = 0; i < 1; i++)
+        for (j = 0; j < 18585; j++)
+            gold[i * out_words_adj + j] = (token_t) 0;
 
-	/* Compute the gold output in software! */
-	printf("Computing Gold output\n");
-	do_decoding(data_bits, cbps, ntraceback, (unsigned char *)in, (unsigned char*)gold);
+    /* Compute the gold output in software! */
+    printf("Computing Gold output\n");
+    do_decoding(data_bits, cbps, ntraceback, (unsigned char *)in, (unsigned char*)gold);
 
-	/* Re-set the input memory ? */
+    /* Re-set the input memory ? */
 
 #if(0)
-	{
-		printf(" memory in   = ");
-		int limi = 0;
-		for (int li = 0; li < 32; li++) {
-			printf("%u", in[limi++]);
-			if ((li % 8) == 7) { printf(" "); }
-		}
-		printf("\n      brtb1    ");
-		for (int li = 0; li < 32; li++) {
-			printf("%u", in[limi++]);
-			if ((li % 8) == 7) { printf(" "); }
-		}
-		printf("\n      depnc    ");
-		for (int li = 0; li < 8; li++) {
-			printf("%u", in[limi++]);
-		}
-		printf("\n      depdta   ");
-		for (int li = 0; li < 32; li++) {
-			printf("%u", in[limi++]);
-			if ((li % 8) == 7) { printf(" "); }
-		}
-		printf("\n");
+    {
+        printf(" memory in   = ");
+        int limi = 0;
+        for (int li = 0; li < 32; li++) {
+            printf("%u", in[limi++]);
+            if ((li % 8) == 7) { printf(" "); }
+        }
+        printf("\n      brtb1    ");
+        for (int li = 0; li < 32; li++) {
+            printf("%u", in[limi++]);
+            if ((li % 8) == 7) { printf(" "); }
+        }
+        printf("\n      depnc    ");
+        for (int li = 0; li < 8; li++) {
+            printf("%u", in[limi++]);
+        }
+        printf("\n      depdta   ");
+        for (int li = 0; li < 32; li++) {
+            printf("%u", in[limi++]);
+            if ((li % 8) == 7) { printf(" "); }
+        }
+        printf("\n");
         }
 #endif
 }
@@ -218,119 +213,120 @@ static void init_buf (token_t *in, token_t * gold)
 
 int main(int argc, char * argv[])
 {
-	int i;
-	int n;
-	int ndev;
-	struct esp_device *espdevs;
-	struct esp_device *dev;
-	unsigned done;
-	unsigned **ptable = NULL;
-	token_t *mem;
-	token_t *gold;
-	unsigned errors = 0;
+    int i;
+    int n;
+    int ndev;
+    struct esp_device *espdevs;
+    struct esp_device *dev;
+    unsigned done;
+    unsigned **ptable = NULL;
+    token_t *mem;
+    token_t *gold;
+    unsigned errors = 0;
 
-	if (DMA_WORD_PER_BEAT(sizeof(token_t)) == 0) {
-		in_words_adj = 24852;
-		out_words_adj = 18585;
-	} else {
-		in_words_adj = round_up(24852, DMA_WORD_PER_BEAT(sizeof(token_t)));
-		out_words_adj = round_up(18585, DMA_WORD_PER_BEAT(sizeof(token_t)));
-	}
-	in_len = in_words_adj * (1);
-	out_len = out_words_adj * (1);
-	in_size = in_len * sizeof(token_t);
-	out_size = out_len * sizeof(token_t);
-	out_offset  = in_len;
-	mem_size = (out_offset * sizeof(token_t)) + out_size;
+    if (DMA_WORD_PER_BEAT(sizeof(token_t)) == 0) {
+        in_words_adj = 24852;
+        out_words_adj = 18585;
+    } else {
+        in_words_adj = round_up(24852, DMA_WORD_PER_BEAT(sizeof(token_t)));
+        out_words_adj = round_up(18585, DMA_WORD_PER_BEAT(sizeof(token_t)));
+    }
+    in_len = in_words_adj * (1);
+    out_len = out_words_adj * (1);
+    in_size = in_len * sizeof(token_t);
+    out_size = out_len * sizeof(token_t);
+    out_offset  = in_len;
+    mem_size = (out_offset * sizeof(token_t)) + out_size;
 
 
-	// Search for the device
-	printf("Scanning device tree... \n");
+    // Search for the device
+    printf("Scanning device tree... \n");
 
-	ndev = probe(&espdevs, VENDOR_SLD, SLD_VITDODEC, DEV_NAME);
-	if (ndev == 0) {
-		printf("vitdodec not found\n");
-		return 0;
-	}
+    ndev = probe(&espdevs, VENDOR_SLD, SLD_VITDODEC, DEV_NAME);
+    if (ndev == 0) {
+        printf("vitdodec not found\n");
+        return 0;
+    }
 
-	for (n = 0; n < ndev; n++) {
+    for (n = 0; n < ndev; n++) {
 
-		dev = &espdevs[n];
+        dev = &espdevs[n];
 
-		// Check DMA capabilities
-		if (ioread32(dev, PT_NCHUNK_MAX_REG) == 0) {
-			printf("  -> scatter-gather DMA is disabled. Abort.\n");
-			return 0;
-		}
+        // Check DMA capabilities
+        if (ioread32(dev, PT_NCHUNK_MAX_REG) == 0) {
+            printf("  -> scatter-gather DMA is disabled. Abort.\n");
+            return 0;
+        }
 
-		if (ioread32(dev, PT_NCHUNK_MAX_REG) < NCHUNK(mem_size)) {
-			printf("  -> Not enough TLB entries available. Abort.\n");
-			return 0;
-		}
+        if (ioread32(dev, PT_NCHUNK_MAX_REG) < NCHUNK(mem_size)) {
+            printf("  -> Not enough TLB entries available. Abort.\n");
+            return 0;
+        }
 
-		// Allocate memory
-		gold = aligned_malloc(out_size);
-		mem = aligned_malloc(mem_size);
-		printf("  memory buffer base-address = %p\n", mem);
-		// Alocate and populate page table
-		ptable = aligned_malloc(NCHUNK(mem_size) * sizeof(unsigned *));
-		for (i = 0; i < NCHUNK(mem_size); i++)
-			ptable[i] = (unsigned *) &mem[i * (CHUNK_SIZE / sizeof(token_t))];
-		printf("  ptable = %p\n", ptable);
-		printf("  nchunk = %lu\n", NCHUNK(mem_size));
+        // Allocate memory
+        gold = aligned_malloc(out_size);
+        mem = aligned_malloc(mem_size);
+        printf("  memory buffer base-address = %p\n", mem);
+        // Alocate and populate page table
+        ptable = aligned_malloc(NCHUNK(mem_size) * sizeof(unsigned *));
+        for (i = 0; i < NCHUNK(mem_size); i++)
+            ptable[i] = (unsigned *) &mem[i * (CHUNK_SIZE / sizeof(token_t))];
+        printf("  ptable = %p\n", ptable);
+        printf("  nchunk = %lu\n", NCHUNK(mem_size));
 
-		printf("  Generate input...\n");
-		init_buf(mem, gold);
+        printf("  Generate input...\n");
+        init_buf(mem, gold);
 
-		// Pass common configuration parameters
+        // Pass common configuration parameters
 
-		iowrite32(dev, SELECT_REG, ioread32(dev, DEVID_REG));
-		iowrite32(dev, COHERENCE_REG, ACC_COH_NONE);
+        iowrite32(dev, SELECT_REG, ioread32(dev, DEVID_REG));
+        iowrite32(dev, COHERENCE_REG, ACC_COH_NONE);
 
-		iowrite32(dev, PT_ADDRESS_REG, (unsigned long) ptable);
+        iowrite32(dev, PT_ADDRESS_REG, (unsigned long) ptable);
 
-		iowrite32(dev, PT_NCHUNK_REG, NCHUNK(mem_size));
-		iowrite32(dev, PT_SHIFT_REG, CHUNK_SHIFT);
+        iowrite32(dev, PT_NCHUNK_REG, NCHUNK(mem_size));
+        iowrite32(dev, PT_SHIFT_REG, CHUNK_SHIFT);
 
-		// Use the following if input and output data are not allocated at the default offsets
-		iowrite32(dev, SRC_OFFSET_REG, 0x0);
-		iowrite32(dev, DST_OFFSET_REG, 0x0);
+        // Use the following if input and output data are not allocated at the default offsets
+        iowrite32(dev, SRC_OFFSET_REG, 0x0);
+        iowrite32(dev, DST_OFFSET_REG, 0x0);
 
-		// Pass accelerator-specific configuration parameters
-		/* <<--regs-config-->> */
-		iowrite32(dev, VITDODEC_CBPS_REG, cbps);
-		iowrite32(dev, VITDODEC_NTRACEBACK_REG, ntraceback);
-		iowrite32(dev, VITDODEC_DATA_BITS_REG, data_bits);
+        // Pass accelerator-specific configuration parameters
+        /* <<--regs-config-->> */
+        iowrite32(dev, VITDODEC_CBPS_REG, cbps);
+        iowrite32(dev, VITDODEC_NTRACEBACK_REG, ntraceback);
+        iowrite32(dev, VITDODEC_DATA_BITS_REG, data_bits);
 
-		// Flush (customize coherence model here)
-		esp_flush(ACC_COH_NONE);
+        // Flush (customize coherence model here)
+        esp_flush(ACC_COH_NONE);
 
-		// Start accelerators
-		printf("  Start...\n");
-		iowrite32(dev, CMD_REG, CMD_MASK_START);
+        // Start accelerators
+        printf("  Start...\n");
+        iowrite32(dev, CMD_REG, CMD_MASK_START);
 
-		// Wait for completion
-		done = 0;
-		while (!done) {
-			done = ioread32(dev, STATUS_REG);
-			done &= STATUS_MASK_DONE;
-		}
-		iowrite32(dev, CMD_REG, 0x0);
+        // Wait for completion
+        done = 0;
+        while (!done) {
+            done = ioread32(dev, STATUS_REG);
+            done &= STATUS_MASK_DONE;
+        }
+        iowrite32(dev, CMD_REG, 0x0);
 
-		printf("  Done\n");
-		printf("  validating...\n");
+        printf("  Done\n");
+        printf("  validating...\n");
 
-		/* Validation */
-		errors = validate_buf(&mem[out_offset], gold);
-		if (errors)
-			printf("  ... FAIL\n");
-		else
-			printf("  ... PASS\n");
+        /* Validation */
+        errors = validate_buf(&mem[out_offset], gold);
+        if (errors)
+            printf("  ... FAIL\n");
+        else
+            printf("  ... PASS\n");
 
-		aligned_free(ptable);
-		aligned_free(mem);
-		aligned_free(gold);
-	}
+        aligned_free(ptable);
+        aligned_free(mem);
+        aligned_free(gold);
+    }
 
-	return 0;
+    return 0;
 }
+
