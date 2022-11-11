d094c73d3bc1725ccb3cc10bc9adbae51cfc033f
Martin Cochet <martin.cochet@ibm.com>
2022-11-11 16:06:26 -0500

    Updated 3x3 LUT, svcf and indexed from 0

--- soft/common/apps/baremetal/token_pm_3x3/token_pm_3x3.c
+++ soft/common/apps/baremetal/token_pm_3x3/token_pm_3x3.c
@@ -32,6 +32,14 @@ int main(int argc, char * argv[])
 	espdevs[i].addr = acc_tile_pm_csr_addr[i];
     }
 
+	//Set NoC LDO to 800Mz - exact value is 800.9MHz 
+	 #ifdef DEBUG
+	 	 printf("Set NoC RO\n");
+	 #endif
+	unsigned int* noc_dco_ptr = (unsigned int *) 0x600903CC;
+	*noc_dco_ptr = 0x5000D;
+
+
 #ifdef TEST_0
 
     reset_token_pm(espdevs);
@@ -439,34 +447,34 @@ int main(int argc, char * argv[])
 
 //Test running all viterbi, NVDLA and FFT tiles
    struct esp_device *espdevs_fft, *espdevs_viterbi, *espdevs_nvdla;
-   struct esp_device *dev_n1 = aligned_malloc(sizeof(struct esp_device));
+   struct esp_device *dev_n0 = aligned_malloc(sizeof(struct esp_device));
    struct esp_device *dev_f1 = aligned_malloc(sizeof(struct esp_device));
    struct esp_device *dev_f2 = aligned_malloc(sizeof(struct esp_device));
-   struct esp_device *dev_f3 = aligned_malloc(sizeof(struct esp_device));
+   struct esp_device *dev_f2 = aligned_malloc(sizeof(struct esp_device));
    struct esp_device *dev_v1 = aligned_malloc(sizeof(struct esp_device));
    struct esp_device *dev_v2 = aligned_malloc(sizeof(struct esp_device));
-   //struct esp_device *dev, *dev_f1, *dev_f2, *dev_f3 ;
-   //struct esp_device *dev_v1, *dev_v2 ;
-   unsigned done_all, done_f1=0, done_v1=0, done_f1_before=0, done_v1_before=0;
-   unsigned done_f2=0, done_v2=0, done_f2_before=0, done_v2_before=0;
-   unsigned done_f3=0, done_n1=0, done_f3_before=0, done_n1_before=0;
+   //struct esp_device *dev, *dev_f0, *dev_f1, *dev_f2 ;
+   //struct esp_device *dev_v0, *dev_v1 ;
+   unsigned done_all, done_f0=0, done_v0=0, done_f0_before=0, done_v0_before=0;
+   unsigned done_f1=0, done_v1=0, done_f1_before=0, done_v1_before=0;
+   unsigned done_f2=0, done_n0=0, done_f2_before=0, done_n0_before=0;
 
+   unsigned **ptable_f0 = NULL;
    unsigned **ptable_f1 = NULL;
    unsigned **ptable_f2 = NULL;
-   unsigned **ptable_f3 = NULL;
+   unsigned **ptable_v0 = NULL;
    unsigned **ptable_v1 = NULL;
-   unsigned **ptable_v2 = NULL;
-   nvdla_token_t *mem_n1;
+   nvdla_token_t *mem_n0;
    nvdla_token_t *gold_nvdla;
-   fft_token_t *mem_f1, *mem_f2, *mem_f3;
+   fft_token_t *mem_f0, *mem_f1, *mem_f2;
    fft_token_t *gold_fft;
-   vit_token_t *mem_v1, *mem_v2;
+   vit_token_t *mem_v0, *mem_v1;
    vit_token_t *gold_vit;
 
    unsigned errors = 0;
    unsigned coherence = ACC_COH_RECALL;
    const int ERROR_COUNT_TH = 0.001;
-   uint64_t cycles_start = 0, cycles_end_n1 = 0, cycles_end_f1 = 0,cycles_end_v1 = 0, cycles_end_f2 = 0, cycles_end_v2 = 0, cycles_end_f3 = 0;
+   uint64_t cycles_start = 0, cycles_end_n0 = 0, cycles_end_f0 = 0,cycles_end_v0 = 0, cycles_end_f1 = 0, cycles_end_v1 = 0, cycles_end_f2 = 0;
    int ndev;
     
   
@@ -529,23 +537,23 @@ int main(int argc, char * argv[])
    mem_size=fft_init_params();
    //fft_probe(&espdevs_fft);
     
-   //dev_f1 = &espdevs_fft[0];
+   //dev_f0 = &espdevs_fft[0];
+   dev_f0->addr = ACC_ADDR_FFT0;
+   setup_fft(dev_f1, gold_fft, mem_f0, ptable_f0);
+   #ifdef DEBUG
+   	printf("FFT0 setup complete, address=0x%x\n",dev_f0->addr);
+   #endif
+   //dev_f1= &espdevs_fft[1];
    dev_f1->addr = ACC_ADDR_FFT1;
    setup_fft(dev_f1, gold_fft, mem_f1, ptable_f1);
    #ifdef DEBUG
    	printf("FFT1 setup complete, address=0x%x\n",dev_f1->addr);
    #endif
-   //dev_f2= &espdevs_fft[1];
+   //dev_f2 = &espdevs_fft[2];
    dev_f2->addr = ACC_ADDR_FFT2;
    setup_fft(dev_f2, gold_fft, mem_f2, ptable_f2);
    #ifdef DEBUG
    	printf("FFT2 setup complete, address=0x%x\n",dev_f2->addr);
-   #endif
-   //dev_f3 = &espdevs_fft[2];
-   dev_f3->addr = ACC_ADDR_FFT3;
-   setup_fft(dev_f3, gold_fft, mem_f3, ptable_f3);
-   #ifdef DEBUG
-   	printf("FFT3 setup complete, address=0x%x\n",dev_f3->addr);
    #endif
    //printf("FFT setup complete\n");
 
@@ -559,18 +567,18 @@ int main(int argc, char * argv[])
     mem_size=vit_init_params();
     //vit_probe(&espdevs_viterbi);
 
-    //dev_v1 = &espdevs_viterbi[0];
-    dev_v1->addr = ACC_ADDR_VITERBI1;
+    //dev_v0 = &espdevs_viterbi[0];
+    dev_v0->addr = ACC_ADDR_VITERBI1;
+    setup_viterbi(dev_v0, gold_vit, mem_v0, ptable_v0);
+    #ifdef DEBUG
+    	printf("Viterbi1 setup complete, address=0x%x\n",dev_v0->addr);
+    #endif
+    //dev_v1= &espdevs_viterbi[1];
+    //dev_v1.addr = ACC_ADDR_VITERBI2;
+    dev_v1->addr = ACC_ADDR_VITERBI2;
     setup_viterbi(dev_v1, gold_vit, mem_v1, ptable_v1);
     #ifdef DEBUG
-    	printf("Viterbi1 setup complete, address=0x%x\n",dev_v1->addr);
-    #endif
-    //dev_v2= &espdevs_viterbi[1];
-    //dev_v2.addr = ACC_ADDR_VITERBI2;
-    dev_v2->addr = ACC_ADDR_VITERBI2;
-    setup_viterbi(dev_v2, gold_vit, mem_v2, ptable_v2);
-    #ifdef DEBUG
-    	printf("Viterbi2 setup complete, address=0x%x\n",dev_v2->addr);
+    	printf("Viterbi2 setup complete, address=0x%x\n",dev_v1->addr);
     #endif
     //printf("Viterbi setup complete\n");
     
@@ -579,8 +587,8 @@ int main(int argc, char * argv[])
     	printf("NVDLA init and start\n");
     #endif
     //nvdla_probe(&espdevs_nvdla);
-    //dev_n1 = &espdevs_nvdla[0];
-    dev_n1->addr = ACC_ADDR_NVDLA;
+    //dev_n0 = &espdevs_nvdla[0];
+    dev_n0->addr = ACC_ADDR_NVDLA;
 
     // Flush (customize coherence model here)
     if (coherence != ACC_COH_RECALL)
@@ -588,35 +596,35 @@ int main(int argc, char * argv[])
 	
     //printf("Start accelerators\n");
     ///////Start accelerators//////
-    iowrite32(dev_f1, CMD_REG, CMD_MASK_START);
+    iowrite32(dev_f0, CMD_REG, CMD_MASK_START);
     espdev = &espdevs[1];
-    write_config1(espdev, activity_const, random_rate_const, 0, 0);
+    //write_config1(espdev, activity_const, random_rate_const, 0, 0);
+    #ifdef DEBUG
+    	printf("Started F0\n");
+    #endif
+    iowrite32(dev_v0, CMD_REG, CMD_MASK_START);
+    espdev = &espdevs[2];
+    //write_config1(espdev, activity_const, random_rate_const, 0, 0);
+    #ifdef DEBUG
+    	printf("Started V0\n");
+    #endif
+    iowrite32(dev_f1, CMD_REG, CMD_MASK_START);
+    espdev = &espdevs[3];
+    //write_config1(espdev, activity_const, random_rate_const, 0, 0);
     #ifdef DEBUG
     	printf("Started F1\n");
     #endif
     iowrite32(dev_v1, CMD_REG, CMD_MASK_START);
-    espdev = &espdevs[2];
-    write_config1(espdev, activity_const, random_rate_const, 0, 0);
+    espdev = &espdevs[4];
+    //write_config1(espdev, activity_const, random_rate_const, 0, 0);
     #ifdef DEBUG
     	printf("Started V1\n");
     #endif
     iowrite32(dev_f2, CMD_REG, CMD_MASK_START);
-    espdev = &espdevs[3];
-    write_config1(espdev, activity_const, random_rate_const, 0, 0);
+    espdev = &espdevs[5];
+    //write_config1(espdev, activity_const, random_rate_const, 0, 0);
     #ifdef DEBUG
     	printf("Started F2\n");
-    #endif
-    iowrite32(dev_v2, CMD_REG, CMD_MASK_START);
-    espdev = &espdevs[4];
-    write_config1(espdev, activity_const, random_rate_const, 0, 0);
-    #ifdef DEBUG
-    	printf("Started V2\n");
-    #endif
-    iowrite32(dev_f3, CMD_REG, CMD_MASK_START);
-    espdev = &espdevs[5];
-    write_config1(espdev, activity_const, random_rate_const, 0, 0);
-    #ifdef DEBUG
-    	printf("Started F3\n");
     #endif
 
     cycles_start = get_counter();
@@ -630,17 +638,21 @@ int main(int argc, char * argv[])
    // }
     espdev = &espdevs[0];
 	write_config1(espdev,activity_const, random_rate_const_0, 0, 0);
-    run_nvdla(espdev, dev_n1, gold_nvdla, mem_n1, 0);
+    run_nvdla(espdev, dev_n0, gold_nvdla, mem_n0, 0);
 	write_config1(espdev, 0, random_rate_const_0, 0, 0);
 
     #ifdef DEBUG
-    	printf("NVDLA finished, address=0x%x\n",dev_n1->addr);
+    	printf("NVDLA finished, address=0x%x\n",dev_n0->addr);
     #endif
 
     ////Wait for them to complete////
     printf("Running all accelerators... Start cycles = %u\n",cycles_start);
     //NVDLA is handled separately... We assume FFT and Viterbi terminate only after NVDLA
-    while (!(done_f1 && done_f2 && done_f3 && done_v1 && done_v2)) {
+    while (!(done_f0 && done_f1 && done_f2 && done_v0 && done_v1)) {
+		if(!done_f0) {
+        		done_f0 = ioread32(dev_f0, STATUS_REG);
+			done_f0 &= STATUS_MASK_DONE;
+		}
 		if(!done_f1) {
         		done_f1 = ioread32(dev_f1, STATUS_REG);
 			done_f1 &= STATUS_MASK_DONE;
@@ -649,23 +661,37 @@ int main(int argc, char * argv[])
         		done_f2 = ioread32(dev_f2, STATUS_REG);
 			done_f2 &= STATUS_MASK_DONE;
 		}
-		if(!done_f3) {
-        		done_f3 = ioread32(dev_f3, STATUS_REG);
-			done_f3 &= STATUS_MASK_DONE;
+		if(!done_v0) {
+        		done_v0 = ioread32(dev_v0, STATUS_REG);
+			done_v0 &= STATUS_MASK_DONE;
 		}
 		if(!done_v1) {
         		done_v1 = ioread32(dev_v1, STATUS_REG);
 			done_v1 &= STATUS_MASK_DONE;
 		}
-		if(!done_v2) {
-        		done_v2 = ioread32(dev_v2, STATUS_REG);
-			done_v2 &= STATUS_MASK_DONE;
-		}
 
+		if(done_f0 && !done_f0_before) {
+			cycles_end_f0 = get_counter();
+			espdev = &espdevs[1];
+			//write_config1(espdev, 0, random_rate_const, 0, 0);
+			done_f0_before=done_f0;
+    			#ifdef DEBUG
+				printf("Finished F0\n");
+    			#endif
+		}
+		if(done_v0 && !done_v0_before) {
+			cycles_end_v0 = get_counter();
+			espdev = &espdevs[2];
+			//write_config1(espdev, 0, random_rate_const, 0, 0);
+			done_v0_before=done_v0;
+    			#ifdef DEBUG
+				printf("Finished V0\n");
+    			#endif
+		}
 		if(done_f1 && !done_f1_before) {
 			cycles_end_f1 = get_counter();
-			espdev = &espdevs[1];
-			write_config1(espdev, 0, random_rate_const, 0, 0);
+			espdev = &espdevs[3];
+			//write_config1(espdev, 0, random_rate_const, 0, 0);
 			done_f1_before=done_f1;
     			#ifdef DEBUG
 				printf("Finished F1\n");
@@ -673,8 +699,8 @@ int main(int argc, char * argv[])
 		}
 		if(done_v1 && !done_v1_before) {
 			cycles_end_v1 = get_counter();
-			espdev = &espdevs[2];
-			write_config1(espdev, 0, random_rate_const, 0, 0);
+			espdev = &espdevs[4];
+			//write_config1(espdev, 0, random_rate_const, 0, 0);
 			done_v1_before=done_v1;
     			#ifdef DEBUG
 				printf("Finished V1\n");
@@ -682,54 +708,36 @@ int main(int argc, char * argv[])
 		}
 		if(done_f2 && !done_f2_before) {
 			cycles_end_f2 = get_counter();
-			espdev = &espdevs[3];
-			write_config1(espdev, 0, random_rate_const, 0, 0);
+			espdev = &espdevs[5];
+			//write_config1(espdev, 0, random_rate_const, 0, 0);
 			done_f2_before=done_f2;
     			#ifdef DEBUG
 				printf("Finished F2\n");
     			#endif
 		}
-		if(done_v2 && !done_v2_before) {
-			cycles_end_v2 = get_counter();
-			espdev = &espdevs[4];
-			write_config1(espdev, 0, random_rate_const, 0, 0);
-			done_v2_before=done_v2;
-    			#ifdef DEBUG
-				printf("Finished V2\n");
-    			#endif
-		}
-		if(done_f3 && !done_f3_before) {
-			cycles_end_f3 = get_counter();
-			espdev = &espdevs[5];
-			write_config1(espdev, 0, random_rate_const, 0, 0);
-			done_f3_before=done_f3;
-    			#ifdef DEBUG
-				printf("Finished F3\n");
-    			#endif
-		}
     }
 	
 	printf("  Done\n");
-	printf("  Viterbi Execution cycles : v1=%llu, v2=%llu\n", cycles_end_v1 - cycles_start,cycles_end_v2 - cycles_start);
-	printf("  FFT Execution cycles : f1=%llu, f2=%llu, f3=%llu\n", cycles_end_f1 - cycles_start,cycles_end_f2 - cycles_start,cycles_end_f3 - cycles_start);
+	printf("  Viterbi Execution cycles : v0=%llu, v1=%llu\n", cycles_end_v0 - cycles_start,cycles_end_v1 - cycles_start);
+	printf("  FFT Execution cycles : f0=%llu, f1=%llu, f2=%llu\n", cycles_end_f0 - cycles_start,cycles_end_f1 - cycles_start,cycles_end_f2 - cycles_start);
 
-	
+	#ifdef DEBUG
     printf("  validating...\n");
 
     // Validation Viterbi
-    errors = vit_validate_buf(&mem_v1[out_offset], gold_vit);
+    errors = vit_validate_buf(&mem_v0[out_offset], gold_vit);
     if (errors)
         printf("  ... FAIL Viterbi\n");
     else
         printf("  ... PASS Viterbi\n");
 
     // Validation FFT
-	errors = fft_validate_buf(&mem_f1[out_offset], gold_fft);
+	errors = fft_validate_buf(&mem_f0[out_offset], gold_fft);
 	if ((errors / len) > ERROR_COUNT_TH)
 	     printf("  ... FAIL FFT\n");
 	 else
 	     printf("  ... PASS FFT\n");
-
+	#endif
 #endif
 
     return 0;
