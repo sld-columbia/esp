09ebfedc1bd3264dbbca33d5fb51f0a356a07acd
Gabriele Tombesi <gt2448@columbia.edu>
2023-03-21 17:55:59 -0400

    :sparkles: MAC example accelerator for Catapult SystemC flow

---
+++ accelerators/catapult_hls/mac_sysc_catapult/hw/inc/mac_conf_info.hpp
@@ -0,0 +1,88 @@
+// Copyright (c) 2011-2023 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef __CONF_INFO_HPP__
+#define __CONF_INFO_HPP__
+
+#pragma once
+
+#include <sstream>
+#include <ac_int.h>
+#include <ac_fixed.h>
+#include "mac_specs.hpp"
+
+//
+// Configuration parameters for the accelerator.
+//
+
+struct conf_info_t
+{
+
+    /* <<--params-->> */
+        int32_t mac_n;
+        int32_t mac_vec;
+        int32_t mac_len;
+
+    static const unsigned int width = 32*3;
+    template <unsigned int Size> void Marshall(Marshaller <Size> &m) {
+        /* <<--marsh-->> */
+        m &mac_n;
+        m &mac_vec;
+        m &mac_len;
+    }
+
+    //
+    // constructors
+    //
+    conf_info_t()
+    {
+        /* <<--ctor-->> */
+        this->mac_n = 1;
+        this->mac_vec = 8;
+        this->mac_len = 16;
+    }
+
+    conf_info_t(
+        /* <<--ctor-args-->> */
+        int32_t mac_n, 
+        int32_t mac_vec, 
+        int32_t mac_len
+        )
+    {
+        /* <<--ctor-custom-->> */
+        this->mac_n = mac_n;
+        this->mac_vec = mac_vec;
+        this->mac_len = mac_len;
+    }
+
+    // VCD dumping function
+   inline friend void sc_trace(sc_trace_file *tf, const conf_info_t &v, const std::string &NAME)
+    {
+        /* <<--sctrc-->> */
+        sc_trace(tf,v.mac_n, NAME + ".mac_n");
+        sc_trace(tf,v.mac_vec, NAME + ".mac_vec");
+        sc_trace(tf,v.mac_len, NAME + ".mac_len");
+
+        // sc_trace(tf,v.mac_n,  NAME + ".mac_n");
+        // sc_trace(tf,v.mac_length,  NAME + ".mac_length");
+        // sc_trace(tf,v.mac_vec,  NAME + ".mac_vec");
+    }
+
+    // redirection operator
+    friend ostream& operator << (ostream& os, conf_info_t const &conf_info)
+    {
+        os << "{";
+        /* <<--print-->> */
+        os << "mac_n = " << conf_info.mac_n << ", ";
+        os << "mac_vec = " << conf_info.mac_vec << ", ";
+        os << "mac_len = " << conf_info.mac_len << "";
+        // os << "mac_n = " << conf_info.mac_n << ", ";
+        // os << "mac_length = " << conf_info.mac_length << ", ";
+        // os << "mac_vec = " << conf_info.mac_vec << ", ";
+        os << "}";
+        return os;
+    }
+
+};
+
+#endif // __MAC_CONF_INFO_HPP__
---
+++ accelerators/catapult_hls/mac_sysc_catapult/hw/inc/mem_bank/DUAL_PORT_RBW_VIRTEXU.tcl
@@ -0,0 +1,47 @@
+#Copyright (c) 2011-2023 Columbia University, System Level Design Group
+#SPDX-License-Identifier: Apache-2.0
+
+flow package require MemGen
+flow run /MemGen/MemoryGenerator_BuildLib {
+VENDOR           Xilinx
+RTLTOOL          Vivado
+TECHNOLOGY       VIRTEX-u
+LIBRARY          DUAL_PORT_RBW
+MODULE           DUAL_PORT_RBW
+OUTPUT_DIR       ../inc/mem_bank
+FILES {
+    { FILENAME ../inc/mem_bank/DUAL_PORT_RBW.v FILETYPE Verilog MODELTYPE generic PARSE 1 PATHTYPE copy STATICFILE 1 VHDL_LIB_MAPS work }
+}
+VHDLARRAYPATH    {}
+WRITEDELAY       0.1
+INITDELAY        1
+READDELAY        0.1
+VERILOGARRAYPATH {}
+TIMEUNIT         1ns
+INPUTDELAY       0.01
+WIDTH            data_width
+AREA             0
+WRITELATENCY     1
+RDWRRESOLUTION   RBW
+READLATENCY      1
+DEPTH            Sz
+PARAMETERS {
+  { PARAMETER AddressSz TYPE hdl IGNORE 0 MIN {} MAX {} DEFAULT 0 }
+  { PARAMETER data_width TYPE hdl IGNORE 0 MIN {} MAX {} DEFAULT 0 }
+  { PARAMETER Sz      TYPE hdl IGNORE 0 MIN {} MAX {} DEFAULT 0 }
+}
+PORTS {
+  { NAME port_0 MODE Read  }
+  { NAME port_1 MODE Write }
+}
+PINMAPS {
+  { PHYPIN clk   LOGPIN CLOCK        DIRECTION in  WIDTH 1.0        PHASE 1  DEFAULT {} PORTS {port_0 port_1} }
+  { PHYPIN clk_en LOGPIN ENABLE       DIRECTION in  WIDTH 1.0        PHASE 1  DEFAULT {} PORTS {port_0 port_1} }
+  { PHYPIN din     LOGPIN DATA_IN      DIRECTION in  WIDTH data_width PHASE {} DEFAULT {} PORTS port_1          }
+  { PHYPIN qout     LOGPIN DATA_OUT     DIRECTION out WIDTH data_width PHASE {} DEFAULT {} PORTS port_0          }
+  { PHYPIN r_adr  LOGPIN ADDRESS      DIRECTION in  WIDTH AddressSz PHASE {} DEFAULT {} PORTS port_0          }
+  { PHYPIN w_adr  LOGPIN ADDRESS      DIRECTION in  WIDTH AddressSz PHASE {} DEFAULT {} PORTS port_1          }
+  { PHYPIN w_en    LOGPIN WRITE_ENABLE DIRECTION in  WIDTH 1.0        PHASE 1  DEFAULT {} PORTS port_1          }
+}
+
+}
