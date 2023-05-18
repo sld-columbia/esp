c186544db4da726e56a7c0f062f7142da2d2f35b
maico <mcassel@cs.columbia.edu>
2023-05-18 09:54:38 -0400

    fix gitignore and copyrights headers

--- accelerators/stratus_hls/sort_stratus/hw/hls/project.tcl
+++ accelerators/stratus_hls/sort_stratus/hw/hls/project.tcl
@@ -1,3 +1,5 @@
+# Copyright (c) 2011-2023 Columbia University, System Level Design Group
+# SPDX-License-Identifier: Apache-2.0
 
 ############################################################
 # Design Parameters
