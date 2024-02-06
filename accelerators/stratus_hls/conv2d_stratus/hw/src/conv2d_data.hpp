2e19baa0272da1eec434078d7a26efe3cc7d8298
Kuan-Lin Chiu <chiu@cs.columbia.edu>
2024-02-05 19:34:33 -0500

    Clang-format for stratus conv2d

--- accelerators/stratus_hls/conv2d_stratus/hw/src/conv2d_data.hpp
+++ accelerators/stratus_hls/conv2d_stratus/hw/src/conv2d_data.hpp
@@ -16,28 +16,28 @@ const unsigned int FPDATA_FL = DATA_WIDTH - FPDATA_IL;
 
 #elif defined(FLOAT_POINT)
 
-#if (DATA_WIDTH == 32)
+    #if (DATA_WIDTH == 32)
 const unsigned int FPDATA_ML = 23;
 const unsigned int FPDATA_EL = 8;
-#elif (DATA_WIDTH == 64)
+    #elif (DATA_WIDTH == 64)
 const unsigned int FPDATA_ML = 52;
 const unsigned int FPDATA_EL = 11;
-#endif
+    #endif
 
 #endif
 
 // Custom SC data types
-typedef sc_dt::sc_uint<2> uint2_t;
-typedef sc_dt::sc_uint<4> uint4_t;
+typedef sc_dt::sc_uint<2>  uint2_t;
+typedef sc_dt::sc_uint<4>  uint4_t;
 typedef sc_dt::sc_uint<12> uint12_t;
 typedef sc_dt::sc_uint<20> uint20_t;
 typedef sc_dt::sc_uint<24> uint24_t;
 typedef sc_dt::sc_uint<28> uint28_t;
-typedef sc_dt::sc_int<2> int2_t;
-typedef sc_dt::sc_int<4> int4_t;
-typedef sc_dt::sc_int<12> int12_t;
-typedef sc_dt::sc_int<20> int20_t;
-typedef sc_dt::sc_int<24> int24_t;
-typedef sc_dt::sc_int<28> int28_t;
+typedef sc_dt::sc_int<2>   int2_t;
+typedef sc_dt::sc_int<4>   int4_t;
+typedef sc_dt::sc_int<12>  int12_t;
+typedef sc_dt::sc_int<20>  int20_t;
+typedef sc_dt::sc_int<24>  int24_t;
+typedef sc_dt::sc_int<28>  int28_t;
 
 #endif // __CONV2D_DATA_HPP__
--- accelerators/stratus_hls/conv2d_stratus/hw/src/fpdata.hpp
+++ accelerators/stratus_hls/conv2d_stratus/hw/src/fpdata.hpp
@@ -11,9 +11,9 @@
 
 #if defined(FIXED_POINT)
 
-// Stratus fixed point
+    // Stratus fixed point
 
-#include "cynw_fixed.h"
+    #include "cynw_fixed.h"
 
 typedef sc_dt::sc_int<WORD_SIZE> FPDATA_WORD;
 
@@ -21,16 +21,14 @@ typedef cynw_fixed<FPDATA_WL, FPDATA_IL> FPDATA;
 
 // Helper functions
 
-template<typename T, size_t N>
-T bv2fp(sc_dt::sc_bv<N> data_in)
+template <typename T, size_t N> T bv2fp(sc_dt::sc_bv<N> data_in)
 {
     T data_out;
 
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "bv2fp1");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "bv2fp-loop");
 
             data_out[i] = data_in[i].to_bool();
@@ -40,14 +38,12 @@ T bv2fp(sc_dt::sc_bv<N> data_in)
     return data_out;
 }
 
-template<typename T, size_t N>
-void bv2fp(T &data_out, sc_dt::sc_bv<N> data_in)
+template <typename T, size_t N> void bv2fp(T &data_out, sc_dt::sc_bv<N> data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "bv2fp2");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "bv2fp-loop");
 
             data_out[i] = data_in[i].to_bool();
@@ -55,36 +51,32 @@ void bv2fp(T &data_out, sc_dt::sc_bv<N> data_in)
     }
 }
 
-template<typename T, size_t N>
-sc_dt::sc_bv<N> fp2bv(T data_in)
+template <typename T, size_t N> sc_dt::sc_bv<N> fp2bv(T data_in)
 {
     sc_dt::sc_bv<N> data_out;
 
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2bv1");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "fp2bv-loop");
 
-            data_out[i] = (bool) data_in[i];
+            data_out[i] = (bool)data_in[i];
         }
     }
 
     return data_out;
 }
 
-template<typename T, size_t N>
-void fp2bv(sc_dt::sc_bv<N> &data_out, T data_in)
+template <typename T, size_t N> void fp2bv(sc_dt::sc_bv<N> &data_out, T data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2bv2");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "fp2bv-loop");
 
-            data_out[i] = (bool) data_in[i];
+            data_out[i] = (bool)data_in[i];
         }
     }
 }
@@ -93,16 +85,14 @@ void fp2bv(sc_dt::sc_bv<N> &data_out, T data_in)
 
 // T <---> sc_dt::sc_int<N>
 
-template<typename T, size_t N>
-T int2fp(sc_dt::sc_int<N> data_in)
+template <typename T, size_t N> T int2fp(sc_dt::sc_int<N> data_in)
 {
     T data_out;
 
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "int2fp1");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "int2fp-loop");
 
             data_out[i] = data_in[i].to_bool();
@@ -112,14 +102,12 @@ T int2fp(sc_dt::sc_int<N> data_in)
     return data_out;
 }
 
-template<typename T, size_t N>
-void int2fp(T &data_out, sc_dt::sc_int<N> data_in)
+template <typename T, size_t N> void int2fp(T &data_out, sc_dt::sc_int<N> data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "int2fp2");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "int2fp-loop");
 
             data_out[i] = data_in[i].to_bool();
@@ -127,45 +115,41 @@ void int2fp(T &data_out, sc_dt::sc_int<N> data_in)
     }
 }
 
-template<typename T, size_t N>
-sc_dt::sc_int<N> fp2int(T data_in)
+template <typename T, size_t N> sc_dt::sc_int<N> fp2int(T data_in)
 {
     sc_dt::sc_int<N> data_out;
 
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2int1");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "fp2int-loop");
 
-            data_out[i] = (bool) data_in[i];
+            data_out[i] = (bool)data_in[i];
         }
     }
 
     return data_out;
 }
 
-template<typename T, size_t N>
-void fp2int(sc_dt::sc_int<N> &data_out, T data_in)
+template <typename T, size_t N> void fp2int(sc_dt::sc_int<N> &data_out, T data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2int2");
 
-        for (unsigned i = 0; i < N; i++)
-        {
+        for (unsigned i = 0; i < N; i++) {
             HLS_UNROLL_LOOP(ON, "fp2int-loop");
 
-            data_out[i] = (bool) data_in[i];
+            data_out[i] = (bool)data_in[i];
         }
     }
 }
 
 #elif defined(FLOAT_POINT)
 
-// Stratus floating point
+    // Stratus floating point
 
-#include "cynw_cm_float.h"
+    #include "cynw_cm_float.h"
 
 typedef sc_dt::sc_uint<WORD_SIZE> FPDATA_WORD;
 
@@ -175,8 +159,7 @@ typedef cynw_cm_float<FPDATA_EL, FPDATA_ML, CYNW_BEST_ACCURACY> FPDATA;
 
 // T <---> sc_dt::sc_uint<N>
 
-template<typename T, size_t N>
-T int2fp(sc_dt::sc_uint<N> data_in)
+template <typename T, size_t N> T int2fp(sc_dt::sc_uint<N> data_in)
 {
     T data_out;
 
@@ -188,8 +171,7 @@ T int2fp(sc_dt::sc_uint<N> data_in)
     }
 }
 
-template<typename T, size_t N>
-void int2fp(T &data_out, sc_dt::sc_uint<N> data_in)
+template <typename T, size_t N> void int2fp(T &data_out, sc_dt::sc_uint<N> data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "uint2fp2");
@@ -198,8 +180,7 @@ void int2fp(T &data_out, sc_dt::sc_uint<N> data_in)
     }
 }
 
-template<typename T, size_t N>
-sc_dt::sc_uint<N> fp2int(T data_in)
+template <typename T, size_t N> sc_dt::sc_uint<N> fp2int(T data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2uint1");
@@ -208,20 +189,18 @@ sc_dt::sc_uint<N> fp2int(T data_in)
     }
 }
 
-template<typename T, size_t N>
-void fp2int(sc_dt::sc_uint<N> &data_out, T data_in)
+template <typename T, size_t N> void fp2int(sc_dt::sc_uint<N> &data_out, T data_in)
 {
-     {
+    {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2uint2");
 
         data_out = sc_dt::sc_uint<N>(data_in.raw_bits());
-     }
+    }
 }
 
 // T <---> sc_dt::sc_bv<N>
 
-template<typename T, size_t N>
-T bv2fp(sc_dt::sc_bv<N> data_in)
+template <typename T, size_t N> T bv2fp(sc_dt::sc_bv<N> data_in)
 {
     T data_out;
 
@@ -233,8 +212,7 @@ T bv2fp(sc_dt::sc_bv<N> data_in)
     }
 }
 
-template<typename T, size_t N>
-void bv2fp(T &data_out, sc_dt::sc_bv<N> data_in)
+template <typename T, size_t N> void bv2fp(T &data_out, sc_dt::sc_bv<N> data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "bv2fp2");
@@ -243,8 +221,7 @@ void bv2fp(T &data_out, sc_dt::sc_bv<N> data_in)
     }
 }
 
-template<typename T, size_t N>
-sc_dt::sc_bv<N> fp2bv(T data_in)
+template <typename T, size_t N> sc_dt::sc_bv<N> fp2bv(T data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2bv1");
@@ -253,8 +230,7 @@ sc_dt::sc_bv<N> fp2bv(T data_in)
     }
 }
 
-template<typename T, size_t N>
-void fp2bv(sc_dt::sc_bv<N> &data_out, T data_in)
+template <typename T, size_t N> void fp2bv(sc_dt::sc_bv<N> &data_out, T data_in)
 {
     {
         HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "fp2bv2");
