91b86f7fd19befad75b1e1910897db85db52eb13
Kuan-Lin Chiu <chiu@cs.columbia.edu>
2024-02-06 14:50:36 -0500

    Clang-format for stratus synth

--- accelerators/stratus_hls/synth_stratus/hw/tb/system.hpp
+++ accelerators/stratus_hls/synth_stratus/hw/tb/system.hpp
@@ -16,16 +16,15 @@
 //#define COLLECT_LATENCY 0
 
 #ifdef CADENCE
-#include "synth_wrap.h"
+    #include "synth_wrap.h"
 #endif
 
 const size_t MEM_SIZE = 3000000; /* Compute memory footprint */
-const size_t N_ACC = 12;
+const size_t N_ACC    = 12;
 
 class system_t : public esp_system<DMA_WIDTH, MEM_SIZE>
 {
-public:
-
+  public:
 #ifdef COLLECT_LATENCY
     put_get_channel<bool> load_chnl;
     put_get_channel<bool> compute_chnl;
@@ -46,40 +45,40 @@ public:
     // Constructor
     SC_HAS_PROCESS(system_t);
     system_t(sc_module_name name)
-	: esp_system<DMA_WIDTH, MEM_SIZE>(name)
+        : esp_system<DMA_WIDTH, MEM_SIZE>(name)
 #ifdef COLLECT_LATENCY
-    , load("load")
-    , compute("compute")
-    , store("store")
+        , load("load")
+        , compute("compute")
+        , store("store")
 #endif
     {
 #ifdef COLLECT_LATENCY
         SC_CTHREAD(report_latency, clk.pos());
         reset_signal_is(acc_rst, false);
 #endif
-        
-    // ACC
+
+        // ACC
 #ifdef CADENCE
-	    acc = new synth_wrapper("synth_wrapper");
+        acc = new synth_wrapper("synth_wrapper");
 #else
-	    acc = new synth("synth_wrapper");
+        acc = new synth("synth_wrapper");
 #endif
-	    // Binding ACC
-	    acc->clk(clk);
-	    acc->rst(acc_rst);
-	    acc->dma_read_ctrl(dma_read_ctrl);
-	    acc->dma_write_ctrl(dma_write_ctrl);
-	    acc->dma_read_chnl(dma_read_chnl);
-	    acc->dma_write_chnl(dma_write_chnl);
-	    acc->conf_info(conf_info);
-	    acc->conf_done(conf_done);
-	    acc->acc_done(acc_done);
-	    acc->debug(debug);
+        // Binding ACC
+        acc->clk(clk);
+        acc->rst(acc_rst);
+        acc->dma_read_ctrl(dma_read_ctrl);
+        acc->dma_write_ctrl(dma_write_ctrl);
+        acc->dma_read_chnl(dma_read_chnl);
+        acc->dma_write_chnl(dma_write_chnl);
+        acc->conf_info(conf_info);
+        acc->conf_done(conf_done);
+        acc->acc_done(acc_done);
+        acc->debug(debug);
 #ifdef COLLECT_LATENCY
         acc->load(load_chnl);
         acc->compute(compute_chnl);
         acc->store(store_chnl);
-        
+
         this->load(load_chnl);
         this->compute(compute_chnl);
         this->store(store_chnl);
@@ -88,7 +87,6 @@ public:
         compute.clk_rst(clk, rst);
         store.clk_rst(clk, rst);
 #endif
-    
     }
 
     // Processes
@@ -105,14 +103,14 @@ public:
     // Send configuration
     void send_config(int acc_id);
     void load_memory_acc_id(int acc_id);
-    int validate_acc_id(int acc_id);
+    int  validate_acc_id(int acc_id);
 
     // Dump internal memory
     void dump_memory();
     void dump_memory_acc_id(int acc_id);
 
     // Validate accelerator results
-    int validate();
+    int            validate();
     sc_signal<int> acc_num;
 
 #ifdef COLLECT_LATENCY
