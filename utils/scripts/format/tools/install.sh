bfbce1f37e1094550bcb40289c813efae2244b7c
Marian Abuhazi <ma4107@columbia.edu>
2024-09-23 03:28:02 +0000

    Comments for install script.

--- utils/scripts/format/tools/install.sh
+++ utils/scripts/format/tools/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Install vsg
 echo "Installing vhdl-style-guide (vsg)..."
 pip3 install vsg --user
 if ! grep -q "$HOME/.local/bin" ~/.bashrc; then
@@ -8,17 +9,20 @@ if ! grep -q "$HOME/.local/bin" ~/.bashrc; then
 fi
 echo "vsg installed and PATH updated."
 
+# Install clang-format-10
 echo ""
 echo "Installing clang-format-10 for C/C++ formatting..."
 sudo apt-get update
 sudo apt-get install -y clang-format-10
 echo "clang-format-10 installed."
 
+# Install autopep8
 echo ""
 echo "Installing autopep8 for Python formatting..."
 pip3 install autopep8 --user
 echo "autopep8 installed."
 
+# Install verible
 echo ""
 echo "Installing Verible for Verilog/SystemVerilog formatting..."
 wget https://github.com/chipsalliance/verible/releases/download/v0.0-3545-ge4028f19/verible-v0.0-3545-ge4028f19-linux-static-x86_64.tar.gz
