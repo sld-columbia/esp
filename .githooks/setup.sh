8d22e8bdb0c55842855710b78d78f2852e142cac
Marian Abuhazi <ma4107@columbia.edu>
2024-09-29 14:37:03 -0400

    Setup script paths

--- .githooks/setup.sh
+++ .githooks/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp $HOME/esp/.githooks/pre-push.sh $HOME/esp/.git/hooks/pre-push
-chmod +x .git/hooks/pre-push
+chmod +x $HOME/esp/.git/hooks/pre-push
 
 echo "Pre-push hook activated successfully."
