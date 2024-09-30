04ae911567d4f2f6030dc7eb86331149b89eb96a
Marian Abuhazi <ma4107@columbia.edu>
2024-09-30 03:17:26 +0000

    Adding error catching for setup

--- .githooks/setup.sh
+++ .githooks/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cp $HOME/esp/.githooks/pre-push.sh $HOME/esp/.git/hooks/pre-push
-chmod +x $HOME/esp/.git/hooks/pre-push
+cp "$HOME/esp/.githooks/pre-push.sh" "$HOME/esp/.git/hooks/pre-push"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to copy pre-push hook."
+    exit 1
+fi
+
+chmod +x "$HOME/esp/.git/hooks/pre-push"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to make pre-push hook executable."
+    exit 1
+fi
 
 echo "Pre-push hook activated successfully."
