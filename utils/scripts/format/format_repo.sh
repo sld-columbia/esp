46442dbd282e1eb88f6d23143687189740312f22
Marian Abuhazi <ma4107@columbia.edu>
2024-10-02 02:01:17 +0000

    Functional format repo scirpt (yay :tada:)

--- utils/scripts/format/format_repo.sh
+++ utils/scripts/format/format_repo.sh
@@ -4,6 +4,8 @@ RED='\033[31m'
 GREEN='\033[32m'
 BOLD='\033[1m'
 RESET='\033[0m'
+BLUE='\033[34m'
+YELLOW='\033[33m'
 
 header() {
     echo ""
@@ -62,62 +64,70 @@ check_tools() {
 }
 
 is_submodule() {
-  local dir="$1"
-  local submodule_paths
+    local dir="$1"
+    local submodule_paths
 
-  submodule_paths=$(git config --file "$cwd"/.gitmodules --get-regexp path | awk '{print $2}')
+    submodule_paths=$(git config --file "$cwd"/.gitmodules --get-regexp path | awk '{print $2}')
 
-  for submodule in $submodule_paths; do
-    if [[ "$dir" == *"$submodule"* ]]; then
-      return 1
+    if [[ "$dir" == *"rtl/caches/esp-caches"* || 
+          "$dir" == *"rtl/caches/spandex-caches"* || 
+          "$dir" == *"accelerators/stratus_hls/common/inc/"* ]]; then
+        return 0
     fi
-  done
-  return 0
+
+    for submodule in $submodule_paths; do
+        if [[ "$dir" == *"$submodule"* ]]; then
+            return 1
+        fi
+    done
+    return 0
 }
 
 cwd="$(git rev-parse --show-toplevel)"
 
 format_all() {
-  local dir="$1"
-  local flags="$2"
-  local extension="$3"
-  local formatter="$4"
+    local dir="$1"
+    local flags="$2"
+    local extension="$3"
+    local formatter="$4"
 
-  for item in "$dir"/*; do
-    echo "$item"
-    if [[ -d "$item" ]]; then
-        if ! is_submodule "$item"; then
-			echo " Skipping git submodule"
-		else
-			format_all "$item" "$flags" "$extension" "$formatter"
-		fi
-    else
-		format_file "$item" "$flags" "$extension" "$formatter"
-    fi
-  done
+    for item in "$dir"/*; do
+        if [[ -d "$item" ]]; then
+            if ! is_submodule "$item"; then
+                echo ""
+                echo -e "${BOLD}${YELLOW}WARNING:${RESET} Submodule skipped - $item"
+            else
+                format_all "$item" "$flags" "$extension" "$formatter"
+            fi
+        else
+            format_file "$item" "$flags" "$extension" "$formatter"
+        fi
+    done
 }
 
-format_file(){
-  local file="$1"
-  local flags="$2"
-  local extension="$3"
-  local formatter="$4"
+format_file() {
+    local file="$1"
+    local flags="$2"
+    local extension="$3"
+    local formatter="$4"
 
-  local output
-  if [[ -f "$file" ]]; then
-        echo " Formatting $(basename "$file")..."
-        # output=$("$formatter" $flags "$file" 2>&1)
-        # local status=$?
+    local output
+    if [[ -f "$file" ]]; then
+        file_extension="${file##*.}"
 
-        # if [[ $status -ne 0 ]] || echo "$output" | grep -qE "warning|error"; then
-        #     echo "___________________________"
-        #     echo ""
-        #     echo -e " - $(basename "$file"): ${RED}${BOLD}ERROR${RESET}"
-        #     echo "$output"
-        #     return 1
-        # else
-        #     echo -e " - $(basename "$file"): ${GREEN}${BOLD}SUCCESS${RESET}"
-        # fi
+        if [[ " ${extension[*]} " == *" $file_extension "* ]]; then
+            echo ""
+            echo -e "${BOLD}${BLUE}INFO:${RESET} Formatting file - $(basename "$file")"
+            output=$("$formatter" $flags "$file" 2>&1)
+            local status=$?
+            if [[ $status -ne 0 ]] || echo "$output" | grep -qE "warning|error"; then
+                echo -n -e "${RED}${BOLD}ERROR${RESET}:"
+                echo "$output"
+                return 1
+            else
+                echo -e " - $(basename "$file"): ${GREEN}${BOLD}SUCCESS${RESET}"
+            fi
+        fi
     fi
 }
 
@@ -127,23 +137,23 @@ assign_formatter() {
     case "$type" in
         c | h | cpp | hpp)
             flags="-i"
-            extension="c h cpp hpp"
+            extension=("c" "h" "cpp" "hpp")
             formatter="clang-format-10"
             ;;
         py)
             flags="-iaaa"
-            extension="py"
+            extension=("py")
             formatter="python3 -m autopep8"
             ;;
         sv | v)
             flags="--inplace --port_declarations_alignment=preserve \
                     --assignment_statement_alignment=align --indentation_spaces=4"
-            extension="sv v"
+            extension=("sv" "v")
             formatter="verible-verilog-format"
             ;;
         vhd)
             flags="--fix -c ~/esp/vhdl-style-guide.yaml"
-            extension="vhd"
+            extension=("vhd")
             formatter="vsg"
             ;;
         *)
@@ -183,16 +193,20 @@ parse_args() {
         esac
         shift
     done
+
     if [[ -z "$flags" || -z "$extension" || -z "$formatter" ]]; then
       usage
       return 1
     else
+      echo -e "Starting formatting process for ${extension[*]} files..."
       format_all "$cwd" "$flags" "$extension" "$formatter"
+	  echo ""
+      echo -e "Formatting process completed."
     fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     header
     check_tools
-    parse_args "$@" >> "output.txt"
+    parse_args "$@"
 fi
