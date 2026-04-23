6b31bb7dab7f1e8848d10a98a433b362f89e7a42
Leonardo Boscolo <lb3712@columbia.edu>
2026-04-23 12:57:02 -0400

    asic: improved vsim.tcl readability and added esp_eda integration to Makefile

--- socs/esp_asic_generic/vsim.tcl
+++ socs/esp_asic_generic/vsim.tcl
@@ -3,10 +3,18 @@ set sdf ""
 set TECHLIB $::env(TECHLIB)
 set ESP_ROOT $::env(ESP_ROOT)
 set VSIMOPT $::env(VSIMOPT)
+
+#####################################################################
+# Uncomment this block of code for RTL simulation using DCO clocking#
+#####################################################################
 #foreach inst [find instances -nodu -bydu DCO] {
 #    append sdf "-sdfmax "
 #    append sdf [string map {( [} [string map {) ]} $inst]]
-#    append sdf "=${ESP_ROOT}/rtl/techmap/${TECHLIB}/dco_wrappers/DCO_tt.sdf "
+#    append sdf "=${ESP_ROOT}/rtl/techmap/asic/dco/DCO_tt.sdf "
 #}
 #append sdf "-suppress 3438"
+#####################################################################
+# End block of code                                                 #
+#####################################################################
+
 eval vsim $sdf $VSIMOPT
