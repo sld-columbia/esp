bb92774516e935794c294542d6109bc8b082dfe5
ShinyMiraidon <114675487+ShinyMiraidon@users.noreply.github.com>
2026-05-15 16:23:23 -0400

    Fix: accgen.sh for hls4ml
    Removed the set -e since it can cause the script to fail when renaming the accelerator in the hls4ml generator flow.

--- tools/accgen/accgen.sh
+++ tools/accgen/accgen.sh
@@ -3,8 +3,6 @@
 # Copyright (c) 2011-2026 Columbia University, System Level Design Group
 # SPDX-License-Identifier: Apache-2.0
 
-set -e
-
 ### Default
 NAME_DEFAULT="dummy"
 ESP_ROOT_DEFAULT=${PWD}
